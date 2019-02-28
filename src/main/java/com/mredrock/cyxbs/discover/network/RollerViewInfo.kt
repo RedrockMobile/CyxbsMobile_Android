51848a373889affd59658e4b770f622a3ba45da5
hosigus <hosigus9733@gmail.com>
2019-02-28 23:40:15 +0800

    合并代码

--- src/main/java/com/mredrock/cyxbs/discover/network/RollerViewInfo.kt
+++ src/main/java/com/mredrock/cyxbs/discover/network/RollerViewInfo.kt
@@ -1,11 +1,13 @@
 package com.mredrock.cyxbs.discover.network
 
+import java.io.Serializable
+
 /**
  * Created by zxzhu
  *   2018/9/7.
  *   enjoy it !!
  */
-class RollerViewInfo {
+class RollerViewInfo : Serializable {
     /**
      * picture_url : http://img.taopic.com/uploads/allimg/120727/201995-120HG1030762.jpg
      * picture_goto_url : www.baidu.com
