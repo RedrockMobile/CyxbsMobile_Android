5e1a1b0b3e484083de4619cb9f419ff0cd622e21
hosigus <hosigus9733@gmail.com>
2019-03-18 20:53:32 +0800

    修复背景音乐退出不停止

--- src/main/java/com/mredrock/cyxbs/discover/map/MapActivity.kt
+++ src/main/java/com/mredrock/cyxbs/discover/map/MapActivity.kt
@@ -3,7 +3,6 @@ package com.mredrock.cyxbs.discover.map
 import android.annotation.SuppressLint
 import android.app.ProgressDialog
 import android.os.Build
-import android.os.Bundle
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -11,30 +10,30 @@ import com.alibaba.android.arouter.facade.annotation.Route
 //import com.jude.swipbackhelper.SwipeBackHelper
 import com.mredrock.cyxbs.common.config.DISCOVER_MAP
 import com.mredrock.cyxbs.common.ui.BaseActivity
-import kotlinx.android.synthetic.main.discover_map_activity_map.*
 
 @Route(path = DISCOVER_MAP)
 class MapActivity : BaseActivity() {
     override val isFragmentActivity = false
+    private lateinit var webView: WebView
 
     @SuppressLint("SetJavaScriptEnabled", "ObsoleteSdkInt")
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.discover_map_activity_map)
+    override fun onStart() {
+        super.onStart()
 
-//        SwipeBackHelper.getCurrentPage(this).setSwipeRelateEnable(false)
+        webView = WebView(this)
+        setContentView(webView)
 
         val dialog = ProgressDialog(this)
         dialog.setMessage("加载中...")
         dialog.show()
 
-        discover_map_web.settings.javaScriptEnabled = true
-        discover_map_web.settings.domStorageEnabled = true
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            discover_map_web.settings.mediaPlaybackRequiresUserGesture = false
+            webView.settings.mediaPlaybackRequiresUserGesture = false
         }
-        discover_map_web.loadUrl("http://720yun.com/t/0e929mp6utn?pano_id=473004")
-        discover_map_web.webChromeClient = object : WebChromeClient() {
+        webView.loadUrl("http://720yun.com/t/0e929mp6utn?pano_id=473004")
+        webView.webChromeClient = object : WebChromeClient() {
             override fun onProgressChanged(view: WebView, newProgress: Int) {
                 super.onProgressChanged(view, newProgress)
                 if (newProgress == 100) {
@@ -42,11 +41,16 @@ class MapActivity : BaseActivity() {
                 }
             }
         }
-        discover_map_web.webViewClient = object : WebViewClient() {
+        webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
                 view.loadUrl(url)
                 return super.shouldOverrideUrlLoading(view, url)
             }
         }
+    }
+
+    override fun onStop() {
+        webView.destroy()
+        super.onStop()
     }
 }
