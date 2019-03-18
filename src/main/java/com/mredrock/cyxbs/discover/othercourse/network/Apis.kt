a5f162d1e09025f60fa1005ffd67ce07ef9358c1
hosigus <hosigus9733@gmail.com>
2019-03-18 20:45:16 +0800

    更换API地址

--- src/main/java/com/mredrock/cyxbs/discover/othercourse/network/Apis.kt
+++ src/main/java/com/mredrock/cyxbs/discover/othercourse/network/Apis.kt
@@ -2,5 +2,5 @@ package com.mredrock.cyxbs.discover.othercourse.network
 
 import com.mredrock.cyxbs.common.config.END_POINT_REDROCK
 
-const val APT_SEARCH_STUDENT = "$END_POINT_REDROCK/cyxbsMobile/index.php/home/searchPeople/peopleList"
+const val APT_SEARCH_STUDENT = "$END_POINT_REDROCK/app/index.php/home/searchPeople/peopleList"
 
