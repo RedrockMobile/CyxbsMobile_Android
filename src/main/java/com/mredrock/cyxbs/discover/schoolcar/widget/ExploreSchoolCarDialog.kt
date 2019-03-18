831df6d31e395f0847ed21e79b24f068eed81acd
cchanges <changes720@outlook.com>
2019-03-18 09:42:28 +0800

    修复对话框拉伸的问题

--- src/main/java/com/mredrock/cyxbs/discover/schoolcar/widget/ExploreSchoolCarDialog.kt
+++ src/main/java/com/mredrock/cyxbs/discover/schoolcar/widget/ExploreSchoolCarDialog.kt
@@ -1,58 +1,65 @@
 package com.mredrock.cyxbs.discover.schoolcar.widget
 
 import android.app.Activity
+import android.graphics.BitmapFactory
 import android.support.v7.app.AlertDialog
+import android.util.Log
 import android.widget.RelativeLayout
+import androidx.annotation.IdRes
 import com.mredrock.cyxbs.schoolcar.R
 import com.mredrock.cyxbs.discover.schoolcar.SchoolCarActivity.Companion.LOST_SERVICES
 import com.mredrock.cyxbs.discover.schoolcar.SchoolCarActivity.Companion.NO_GPS
 import com.mredrock.cyxbs.discover.schoolcar.SchoolCarActivity.Companion.TIME_OUT
-import kotlinx.android.synthetic.main.explore_schoolcar_notserve_dialog.view.*
+import kotlinx.android.synthetic.main.schoolcar_notserve_dialog.view.*
 
 /**
  * Created by glossimar on 2018/9/12
  */
 
-class  ExploreSchoolCarDialog {
+class ExploreSchoolCarDialog {
 
     companion object {
         private var dialog: AlertDialog? = null
 
-        fun show(activity: Activity, type: Int){
+        fun show(activity: Activity, type: Int) {
 
             val inflater = activity.layoutInflater
-            val layout = inflater.inflate(R.layout.explore_schoolcar_notserve_dialog, null) as RelativeLayout
+            val layout = inflater.inflate(R.layout.schoolcar_notserve_dialog, null) as RelativeLayout
 
-            dialog = AlertDialog.Builder(activity).
-                    setCancelable(true).
-                    setView(layout).
-                    create()
+            dialog = AlertDialog.Builder(activity).setCancelable(true).setView(layout).create()
             layout.school_car_dialog_dismiss_button.setOnClickListener {
                 if (it.id == R.id.school_car_dialog_dismiss_button) {
                     dialog!!.dismiss()
                 }
             }
 
-            try {
-                val manager = activity.windowManager
-                val width = manager.defaultDisplay.width
-                val height = manager.defaultDisplay.height
-
-                val dialogWindow = dialog!!.window
-                dialog!!.show()
-                dialogWindow!!.setLayout(width * 4 / 5, height * 7 / 11)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+//            try {
+//                val manager = activity.windowManager
+//                val width = manager.defaultDisplay.width
+//                val height = manager.defaultDisplay.height
+//
+//                val dialogWindow = dialog!!.window
+//                dialog!!.show()
+//                dialogWindow!!.setLayout(width * 4 / 5, height * 7 / 11)
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
 
             try {
                 when (type) {
-                    LOST_SERVICES -> dialog!!.window!!.setBackgroundDrawableResource(R.drawable.dialog_school_car_not_serve)
-                    TIME_OUT -> dialog!!.window!!.setBackgroundDrawableResource(R.drawable.ic_school_car_search_time_out)
+                    LOST_SERVICES -> {
+                        setLayout(activity,R.drawable.dialog_school_car_not_serve)
+                        dialog!!.window!!.setBackgroundDrawableResource(R.drawable.dialog_school_car_not_serve)
+                    }
+                    TIME_OUT -> {
+                        setLayout(activity,R.drawable.ic_school_car_search_time_out)
+                        dialog!!.window!!.setBackgroundDrawableResource(R.drawable.ic_school_car_search_time_out)
+                    }
                     NO_GPS -> {
+                        setLayout(activity,R.drawable.ic_school_car_search_no_gps)
                         dialog!!.window.setBackgroundDrawableResource(R.drawable.ic_school_car_search_no_gps)
                         layout.school_car_dialog_negative_button.setOnClickListener { dialog!!.cancel() }
-                        layout.school_car_dialog_positive_button.setOnClickListener { dialog!!.cancel()  }
+                        layout.school_car_dialog_positive_button.setOnClickListener { dialog!!.cancel() }
                     }
                 }
             } catch (e: IllegalArgumentException) {
@@ -62,7 +69,21 @@ class  ExploreSchoolCarDialog {
             }
         }
 
-        fun cancelDialog(){
+        //防止对话框拉伸，直接获取drawable对象的宽高之后进行加载
+        private fun setLayout(activity: Activity,drawableId: Int) {
+            val op = BitmapFactory.Options()
+            op.inJustDecodeBounds = true
+            BitmapFactory.decodeResource(activity.resources,drawableId,op)
+            val dm = activity.resources.displayMetrics
+            val width = dm.widthPixels * 4 / 5
+            val multiple = op.outHeight.toFloat() / op.outWidth
+            val height = width * multiple
+            val dialogWindow = dialog!!.window
+            dialog!!.show()
+            dialogWindow!!.setLayout(width, height.toInt())
+        }
+
+        fun cancelDialog() {
             try {
                 if (dialog != null && dialog!!.isShowing) {
                     dialog!!.dismiss()
