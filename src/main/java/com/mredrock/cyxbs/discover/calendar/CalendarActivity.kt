8c6bbe3cec60b50ca3c1351e1d3e90260547397f
hosigus <hosigus9733@gmail.com>
2019-04-22 20:16:30 +0800

    更改后端接口
    Signed-off-by: hosigus <hosigus9733@gmail.com>

--- src/main/java/com/mredrock/cyxbs/discover/calendar/CalendarActivity.kt
+++ src/main/java/com/mredrock/cyxbs/discover/calendar/CalendarActivity.kt
@@ -1,48 +1,23 @@
 package com.mredrock.cyxbs.discover.calendar
 
 import android.os.Bundle
-import android.util.Log
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.bumptech.glide.Glide
 import com.mredrock.cyxbs.calendar.R
+import com.mredrock.cyxbs.common.config.BASE_NORMAL_IMG_URL
 import com.mredrock.cyxbs.common.config.DISCOVER_CALENDAR
-import com.mredrock.cyxbs.common.network.ApiGenerator
 import com.mredrock.cyxbs.common.ui.BaseActivity
-import com.mredrock.cyxbs.common.utils.extensions.doOnErrorWithDefaultErrorHandler
-import com.mredrock.cyxbs.common.utils.extensions.mapOrThrowApiException
-import com.mredrock.cyxbs.common.utils.extensions.safeSubscribeBy
-import com.mredrock.cyxbs.common.utils.extensions.setSchedulers
-import com.mredrock.cyxbs.discover.calendar.network.ApiService
 import kotlinx.android.synthetic.main.calendar_activity_main.*
-import org.jetbrains.anko.toast
 
 @Route(path = DISCOVER_CALENDAR)
 class CalendarActivity : BaseActivity() {
 
-    private val apiService = ApiGenerator.getApiService(ApiService::class.java)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calendar_activity_main)
         common_toolbar.init("校 历")
-        init()
+        Glide.with(this).load(BASE_NORMAL_IMG_URL + "schoolCalendar").into(iv_calendar)
     }
 
-    override val isFragmentActivity = true
-    private fun init() {
-        apiService
-                .getCalendar()
-                .mapOrThrowApiException()
-                .setSchedulers()
-                .doOnErrorWithDefaultErrorHandler {
-                    toast("无法获取校历")
-                    Log.d("Calendar", it.localizedMessage)
-                    false
-                }
-                .safeSubscribeBy { it ->
-                    if (it.isNotEmpty()) {
-                        Glide.with(this).load(it.get(0).address).into(iv_calendar1)
-                        Glide.with(this).load(it.get(1).address).into(iv_calendar2)
-                    }
-                }
-    }
+    override val isFragmentActivity = false
 }
--- src/main/java/com/mredrock/cyxbs/discover/calendar/network/Calendar.kt
+++
@@ -1,5 +0,0 @@
-package com.mredrock.cyxbs.discover.calendar.network
-
-import java.io.Serializable
-
-data class Calendar(val address: String, val name: String, val property: String) : Serializable
