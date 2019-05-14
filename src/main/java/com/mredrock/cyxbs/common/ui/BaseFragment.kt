f95d6358f1f2e6063854a6ea4c46941533b7483a
jay68 <1432562823l@gmail.com>
2019-05-14 22:54:50 +0800

    修复友盟统计

--- src/main/java/com/mredrock/cyxbs/common/ui/BaseFragment.kt
+++ src/main/java/com/mredrock/cyxbs/common/ui/BaseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.View
 import com.mredrock.cyxbs.common.event.LoginStateChangeEvent
+import com.mredrock.cyxbs.common.utils.LogUtils
 import com.umeng.analytics.MobclickAgent
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
@@ -16,28 +17,38 @@ open class BaseFragment : Fragment() {
     //是否打开友盟统计，当此Fragment只是其他fragment容器时，应当考虑关闭统计
     protected open val openStatistics = true
 
-    override fun onResume() {
-        super.onResume()
-        if (openStatistics) {
-            MobclickAgent.onPageStart(javaClass.name)
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (openStatistics) {
-            MobclickAgent.onPageEnd(javaClass.name)
-        }
-    }
+    private var isStarted = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         EventBus.getDefault().register(this)
     }
 
+    override fun onPause() {
+        super.onPause()
+        //在退出当前页面时需要手动调用setUserVisibleHint方法
+        userVisibleHint = false
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         EventBus.getDefault().unregister(this)
+    }
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        if (!openStatistics) {
+            return
+        }
+        if (!isStarted && isVisibleToUser) {
+            isStarted = true
+            MobclickAgent.onPageStart(javaClass.name)
+            LogUtils.d("UMStat",  javaClass.name + " started")
+        } else if (isStarted) {
+            isStarted = false
+            MobclickAgent.onPageEnd(javaClass.name)
+            LogUtils.d("UMStat",  javaClass.name + " paused")
+        }
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
