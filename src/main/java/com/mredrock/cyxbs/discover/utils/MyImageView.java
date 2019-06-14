147240e94615ad3d54ecd72f182de15a05e88b20
hosigus <hosigus9733@gmail.com>
2019-06-14 19:19:03 +0800

    width and height must be > 0

--- src/main/java/com/mredrock/cyxbs/discover/utils/MyImageView.java
+++ src/main/java/com/mredrock/cyxbs/discover/utils/MyImageView.java
@@ -68,7 +68,7 @@ public class MyImageView extends AppCompatImageView {
     @Override
     public void draw(Canvas canvas) {
 
-        Bitmap bitmap = Bitmap.createBitmap(getWidth()+1, getHeight(), Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = Bitmap.createBitmap(getWidth() + 1, getHeight() + 1, Bitmap.Config.ARGB_8888);
         Canvas canvas2 = new Canvas(bitmap);
         super.draw(canvas2);
         drawLiftUp(canvas2);
