812dbced117ad506aba545b309421c979fbc8d48
Hosigus <973367033@qq.com>
2019-08-22 11:58:45 +0800

    optimize codes
    Signed-off-by: Hosigus <973367033@qq.com>

--- src/androidTest/java/com/mredrock/cyxbs/discover/map/ExampleInstrumentedTest.kt
+++ src/androidTest/java/com/mredrock/cyxbs/discover/map/ExampleInstrumentedTest.kt
@@ -2,11 +2,9 @@ package com.mredrock.cyxbs.discover.map
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
