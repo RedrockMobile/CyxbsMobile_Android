bae1a9ee992db22a7a378916c928af706a501a99
Fxy4ever <381391264@qq.com>
2019-09-28 23:26:16 +0800

    [BugFix] 优化掌邮启动页面以及闪屏页加载逻辑 1、 将Application中大多数的三方库改为异步初始化。 2、 将SplashActivity中的加载逻辑移动到了MainActivity，在MainActivity中对比、下载新图片，将url对比防止重复下载。后期若想取消闪屏页推送，返回非http字符串即可。 Signed-off-by: Fxy4ever <381391264@qq.com>

--- src/main/java/com/mredrock/cyxbs/common/BaseApp.kt
+++ src/main/java/com/mredrock/cyxbs/common/BaseApp.kt
@@ -8,7 +8,6 @@ import com.google.gson.Gson
 import com.meituan.android.walle.WalleChannelReader
 import com.mredrock.cyxbs.common.bean.User
 import com.mredrock.cyxbs.common.config.SP_KEY_USER
-import com.mredrock.cyxbs.common.utils.CrashHandler
 import com.mredrock.cyxbs.common.utils.LogUtils
 import com.mredrock.cyxbs.common.utils.encrypt.UserInfoEncryption
 import com.mredrock.cyxbs.common.utils.extensions.defaultSharedPreferences
@@ -62,10 +61,10 @@ open class BaseApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        CrashHandler.init(applicationContext)
-        userInfoEncryption = UserInfoEncryption()
-        initRouter()
+        BaseAppInitService.init(applicationContext)
+        initRouter()//ARouter放在子线程会影响使用
         initUMeng()
+        userInfoEncryption = UserInfoEncryption()
     }
 
     private fun initRouter() {
@@ -76,11 +75,12 @@ open class BaseApp : Application() {
         ARouter.init(this)
     }
 
+
     private fun initUMeng() {
-        val channel = WalleChannelReader.getChannel(this, "debug")
+        val channel = WalleChannelReader.getChannel(applicationContext, "debug")
         UMConfigure.init(applicationContext, BuildConfig.UM_APP_KEY, channel, UMConfigure.DEVICE_TYPE_PHONE,
                 BuildConfig.UM_PUSH_SECRET)
-        MobclickAgent.setScenarioType(this, MobclickAgent.EScenarioType.E_UM_NORMAL)
+        MobclickAgent.setScenarioType(applicationContext, MobclickAgent.EScenarioType.E_UM_NORMAL)
         MobclickAgent.openActivityDurationTrack(false)
         //调试模式（推荐到umeng注册测试机，避免数据污染）
         UMConfigure.setLogEnabled(BuildConfig.DEBUG)
