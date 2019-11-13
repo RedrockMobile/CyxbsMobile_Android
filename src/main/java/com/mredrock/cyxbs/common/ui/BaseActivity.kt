02bb504f40212a5baad9f00ad0ee167ad91cf6ae
Jon <1246634075@qq.com>
2019-11-13 20:17:36 +0800

    [Future]添加debug x86
    # Conflicts:
    #	src/main/java/com/mredrock/cyxbs/main/ui/MainActivity.kt
    #	src/main/res/layout/main_activity_main.xml

--- src/main/java/com/mredrock/cyxbs/common/ui/BaseActivity.kt
+++ src/main/java/com/mredrock/cyxbs/common/ui/BaseActivity.kt
@@ -2,6 +2,7 @@ package com.mredrock.cyxbs.common.ui
 
 //import com.jude.swipbackhelper.SwipeBackHelper
 import android.app.Activity
+import android.content.Context
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
@@ -47,14 +48,15 @@ abstract class BaseActivity : AppCompatActivity() {
 //        SwipeBackHelper.onCreate(this)
 //        SwipeBackHelper.getCurrentPage(this).setSwipeRelateEnable(true)
 
-        if (Build.VERSION.SDK_INT >= 21) {
-            val decorView = window.decorView
-            val option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            decorView.systemUiVisibility = option
-//            window.navigationBarColor = Color.TRANSPARENT
-            window.statusBarColor = Color.TRANSPARENT
-        } else if (Build.VERSION.SDK_INT >= 19) {
-            window.setFlags(
+        when {
+            Build.VERSION.SDK_INT >= 23 -> window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            Build.VERSION.SDK_INT >= 21 -> {
+                val decorView = window.decorView
+                val option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                decorView.systemUiVisibility = option
+                window.statusBarColor = Color.TRANSPARENT
+            }
+            Build.VERSION.SDK_INT >= 19 -> window.setFlags(
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
             )
