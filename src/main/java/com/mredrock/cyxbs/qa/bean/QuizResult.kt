e07caa766ed261199914bea06b36c516fd0c1de7
yyfbe <2861112448@qq.com>
2020-01-30 19:23:00 +0800

    [BugFix]修改提问返回字段名称

--- src/main/java/com/mredrock/cyxbs/qa/bean/QuizResult.kt
+++ src/main/java/com/mredrock/cyxbs/qa/bean/QuizResult.kt
@@ -1,8 +1,9 @@
 package com.mredrock.cyxbs.qa.bean
 
+import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 /**
  * Created By jay68 on 2018/12/3.
  */
-data class QuizResult(val id: String = "") : Serializable
+data class QuizResult(@SerializedName("Id") val id: String = "") : Serializable
