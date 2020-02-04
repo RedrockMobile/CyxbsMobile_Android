111f77de099b7b5892458572b46203d06c7f7c2f
zixuan <931717954@qq.com>
2020-02-04 16:19:15 +0800

    [Upgrade]优化了在发现页上的信息碎片的生命周期控制，减决了刷新问题
    Signed-off-by: zixuan <931717954@qq.com>

--- src/main/java/com/mredrock/cyxbs/volunteer/fragment/DiscoverVolunteerFeedFragment.kt
+++ src/main/java/com/mredrock/cyxbs/volunteer/fragment/DiscoverVolunteerFeedFragment.kt
@@ -1,6 +1,8 @@
 package com.mredrock.cyxbs.volunteer.fragment
 
 import android.app.Activity
+import android.os.Bundle
+import android.view.View
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.mredrock.cyxbs.common.config.DISCOVER_VOLUNTEER
@@ -19,23 +21,26 @@ class DiscoverVolunteerFeedFragment : BaseFeedFragment<DiscoverVolunteerFeedView
 
     override val viewModelClass: Class<DiscoverVolunteerFeedViewModel> = DiscoverVolunteerFeedViewModel::class.java
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setAdapter(VolunteerFeedUnbindAdapter())
+    }
+
     override fun onResume() {
         super.onResume()
+        init()
+    }
 
+    private fun init() {
         setTitle("志愿服务")
         val volunteerSP = VolunteerTimeSP(activity as Activity)
         val uid = volunteerSP.volunteerUid
-        setAdapter(VolunteerFeedUnbindAdapter())
+
         if (!(uid == "404" || volunteerSP.volunteerAccount == "404" ||
                         volunteerSP.volunteerPassword == "404")) {
 
             viewModel.loadVolunteerTime(EncryptPassword.encrypt(uid))
         }
-        init()
-    }
-
-    private fun init() {
-
         viewModel.volunteerData.observe { volunteerTime ->
 
             volunteerTime?.let {
