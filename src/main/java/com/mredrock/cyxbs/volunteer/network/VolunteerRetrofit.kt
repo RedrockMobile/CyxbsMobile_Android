6407c0b726a121d33af5967957db2139a3110ec0
Jon <1246634075@qq.com>
2020-02-08 19:27:46 +0800

    [BugFix]合并

--- src/main/java/com/mredrock/cyxbs/volunteer/network/VolunteerRetrofit.kt
+++ src/main/java/com/mredrock/cyxbs/volunteer/network/VolunteerRetrofit.kt
@@ -1,15 +1,8 @@
 package com.mredrock.cyxbs.volunteer.network
 
 import com.mredrock.cyxbs.common.BuildConfig
-import com.mredrock.cyxbs.common.network.ApiGenerator
-import com.mredrock.cyxbs.common.network.ApiService
-import com.mredrock.cyxbs.common.network.converter.QualifiedTypeConverterFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory
 import java.util.concurrent.TimeUnit
 
 class VolunteerRetrofit {
