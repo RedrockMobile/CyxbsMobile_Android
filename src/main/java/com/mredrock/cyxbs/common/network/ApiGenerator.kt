acf530673855f64f26001c42f38b3bcbb7764abf
yyfbe <2861112448@qq.com>
2020-02-16 15:14:27 +0800

    [BugFix]修改token刷新逻辑

--- src/main/java/com/mredrock/cyxbs/common/network/ApiGenerator.kt
+++ src/main/java/com/mredrock/cyxbs/common/network/ApiGenerator.kt
@@ -24,9 +24,7 @@ object ApiGenerator {
     private var retrofit: Retrofit
     private var commonRetrofit: Retrofit
     private var okHttpClient: OkHttpClient
-    @Volatile
     var token = ""
-    @Volatile
     var refreshToken = ""
 
     init {
@@ -49,10 +47,15 @@ object ApiGenerator {
             connectTimeout(DEFAULT_TIME_OUT.toLong(), TimeUnit.SECONDS)
             interceptors().add(Interceptor {
                 /**
-                 * 所有请求添加token到header，如果有更好方式再改改，或者另外加一个不需要token的ApiGenerator
+                 * 所有请求添加token到header
+                 * 在外面加一层判断，用于token未过期时，能够异步请求，不用阻塞在checkRefresh()
+                 * 如果有更好方式再改改
                  */
-                val response = it.proceed(it.request().newBuilder().header("Authorization", "Bearer $token").build())
-                checkRefresh(response, it)
+                if (refreshToken.isNotEmpty() && isTokenExpired()) {
+                    checkRefresh(it)
+                } else {
+                    it.proceed(it.request().newBuilder().header("Authorization", "Bearer $token").build())
+                }
             })
         }
         if (BuildConfig.DEBUG) {
@@ -64,27 +67,27 @@ object ApiGenerator {
     }
 
     @Synchronized
-    private fun checkRefresh(response: Response, chain: Interceptor.Chain): Response? {
-        var newResponse = response
+    private fun checkRefresh(chain: Interceptor.Chain): Response? {
+        var response = chain.proceed(chain.request().newBuilder().header("Authorization", "Bearer $token").build())
         /**
          * 刷新token条件设置为，已有refreshToken，并且已经过期，也可以后端返回特定到token失效code
+         * 当第一个过期token请求接口后，改变token和refreshToken，防止同步refreshToken失效
+         * 之后进入该方法的请求，token已经刷新
          */
         if (refreshToken.isNotEmpty() && isTokenExpired()) {
             ServiceManager.getService(IAccountService::class.java).getVerifyService().refresh(
                     onError = {
                         response.close()
-                        newResponse = response
                     },
                     action = { s: String, s1: String ->
                         response.close()
                         token = s
                         refreshToken = s1
-                        newResponse = chain.run { proceed(chain.request().newBuilder().header("Authorization", "Bearer $s").build()) }
-
+                        response = chain.run { proceed(chain.request().newBuilder().header("Authorization", "Bearer $s").build()) }
                     }
             )
         }
-        return newResponse
+        return response
     }
 
     private fun commonApiService(): Retrofit {
