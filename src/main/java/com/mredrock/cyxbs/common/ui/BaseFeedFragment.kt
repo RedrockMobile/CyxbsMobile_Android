89b98f2bb537212afd18ed4e17f68b69c64701ac
zixuan <931717954@qq.com>
2020-03-03 18:08:57 +0800

    [Upgrade]重构信息页的通用adapter，调整信息页分割线添加方式
    Signed-off-by: zixuan <931717954@qq.com>

--- src/main/java/com/mredrock/cyxbs/common/ui/BaseFeedFragment.kt
+++ src/main/java/com/mredrock/cyxbs/common/ui/BaseFeedFragment.kt
@@ -2,6 +2,7 @@ package com.mredrock.cyxbs.common.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,14 +11,22 @@ import com.mredrock.cyxbs.common.viewmodel.BaseViewModel
 import kotlinx.android.synthetic.main.fragment_base_feed.*
 import org.jetbrains.anko.textResource
 
+//module_discover 中的信息部分
 abstract class BaseFeedFragment<T : BaseViewModel> : BaseViewModelFragment<T>() {
     private lateinit var adapter: Adapter
+    protected abstract var hasTopSplitLine:Boolean
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_base_feed, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(hasTopSplitLine){
+            feed_split_line.visibility = View.VISIBLE
+        }
+    }
 
     fun setTitle(name: String) {
         tv_feed_title.text = name
@@ -25,6 +34,11 @@ abstract class BaseFeedFragment<T : BaseViewModel> : BaseViewModelFragment<T>() 
 
     fun setTitle(res: Int) {
         tv_feed_title.textResource = res
+    }
+
+    override fun onResume() {
+        super.onResume()
+        onRefresh()
     }
 
     fun setSubtitle(name: String) {
@@ -37,22 +51,32 @@ abstract class BaseFeedFragment<T : BaseViewModel> : BaseViewModelFragment<T>() 
 
     fun setAdapter(adapter: Adapter) {
         this.adapter = adapter
-        if (ll_feed.childCount > 1) {
-            ll_feed.removeViews(1, ll_feed.childCount - 1)
+        TransitionManager.beginDelayedTransition(ll_feed)
+        if (ll_feed.childCount > 2) {
+            ll_feed.removeViews(2, ll_feed.childCount - 2)
         }
         context?.let {
             ll_feed.addView(adapter.getView(it, ll_feed))
-            layoutInflater?.inflate(R.layout.common_splite_line, ll_feed)
         }
 
     }
+    fun getAdapter() = adapter
 
     fun setOnClickListener(listener: () -> Unit) {
         ll_feed.setOnClickListener { listener.invoke() }
     }
 
-    interface Adapter {
-        fun getView(context: Context, parent: ViewGroup): View
+    abstract fun onRefresh()
+
+    abstract class Adapter{
+        protected var view:View? = null
+        fun getView(context: Context,parent: ViewGroup):View{
+            view = view?:onCreateView(context,parent)
+            return view!!
+        }
+
+        abstract fun onCreateView(context: Context, parent: ViewGroup): View
+
     }
 }
 
