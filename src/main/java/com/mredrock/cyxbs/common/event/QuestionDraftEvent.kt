f523f8644e1b9ed43e51ed5dd76f12336e56493a
yyfbe <2861112448@qq.com>
2020-03-10 10:02:38 +0800

    [Bugfix]修改草稿箱EventBus

--- src/main/java/com/mredrock/cyxbs/common/event/QuestionDraftEvent.kt
+++ src/main/java/com/mredrock/cyxbs/common/event/QuestionDraftEvent.kt
@@ -3,4 +3,4 @@ package com.mredrock.cyxbs.common.event
 /**
  * Created by Cchanges on 2019/3/22
  */
-class DraftEvent(val jsonString: String, val selfId: String, val targetId: String)
+class QuestionDraftEvent(val jsonString: String, val selfId: String)
