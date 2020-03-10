d3de83568a5d1bd8e15deb1162a51e4097c3bb61
yyfbe <2861112448@qq.com>
2020-03-10 11:33:08 +0800

    [Optimize]优化草稿箱EventBus，便于后期修改

--- src/main/java/com/mredrock/cyxbs/common/event/AnswerDraftEvent.kt
+++ src/main/java/com/mredrock/cyxbs/common/event/AnswerDraftEvent.kt
@@ -3,4 +3,4 @@ package com.mredrock.cyxbs.common.event
 /**
  * Created by yyfbe, Date on 2020/3/9.
  */
-class AnswerDraftEvent(val jsonString: String, val selfId: String, val targetId: String, val questionContent: String, val questionPics: ArrayList<String>)
+class AnswerDraftEvent(val jsonString: String, val selfId: String, val targetId: String)
