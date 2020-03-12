826f442cc03997577012c5abbf6de11d81c9980a
Jon <1246634075@qq.com>
2020-03-13 01:34:10 +0800

    [Feature]解决校历长图模糊的问题

--- src/main/java/com/mredrock/cyxbs/discover/calendar/CalendarActivity.kt
+++ src/main/java/com/mredrock/cyxbs/discover/calendar/CalendarActivity.kt
@@ -1,18 +1,19 @@
 package com.mredrock.cyxbs.discover.calendar
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.ViewGroup
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DecodeFormat
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.DrawableImageViewTarget
+import com.bumptech.glide.request.target.ImageViewTarget
+import com.bumptech.glide.request.transition.Transition
 import com.mredrock.cyxbs.calendar.R
 import com.mredrock.cyxbs.common.config.DISCOVER_CALENDAR
 import com.mredrock.cyxbs.common.config.END_POINT_REDROCK
 import com.mredrock.cyxbs.common.ui.BaseActivity
-import com.mredrock.cyxbs.common.utils.extensions.getScreenHeight
-import com.mredrock.cyxbs.common.utils.extensions.getScreenWidth
 import kotlinx.android.synthetic.main.calendar_activity_main.*
+
 
 @Route(path = DISCOVER_CALENDAR)
 
@@ -22,16 +23,23 @@ class CalendarActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calendar_activity_main)
-        common_toolbar.init("校 历")
-        val request = RequestOptions()
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
-                .override(getScreenWidth(), getScreenHeight())
-                .format(DecodeFormat.PREFER_ARGB_8888)
-                .fitCenter()
         Glide.with(this)
                 .load("$END_POINT_REDROCK/234/newapi/schoolCalendar")
-                .apply(request)
-                .into(iv_calendar)
+                .into(object : DrawableImageViewTarget(iv_calendar) {
+                    override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                        val width: Int = resource.intrinsicWidth
+                        val height: Int = resource.intrinsicHeight
+                        var ivWidth: Int = iv_calendar.width
+                        if (ivWidth == 0) {
+                            ivWidth = iv_calendar.resources.displayMetrics.widthPixels
+                        }
+                        val ivHeight = (height / width * ivWidth)
+                        val lp: ViewGroup.LayoutParams = iv_calendar.getLayoutParams()
+                        lp.height = ivHeight
+                        iv_calendar.layoutParams = lp
+                        iv_calendar.setImageDrawable(resource)
+                    }
+                })
     }
 
     override val isFragmentActivity = false
