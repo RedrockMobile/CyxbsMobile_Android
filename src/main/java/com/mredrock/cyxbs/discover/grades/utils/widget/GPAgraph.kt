004ebd5ac8a06874565dff28f4a5ff8c1497b2aa
Jon <1246634075@qq.com>
2020-03-18 15:37:52 +0800

    [Optimize]优化页面跳转动画

--- src/main/java/com/mredrock/cyxbs/discover/grades/utils/widget/GPAgraph.kt
+++ src/main/java/com/mredrock/cyxbs/discover/grades/utils/widget/GPAgraph.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
