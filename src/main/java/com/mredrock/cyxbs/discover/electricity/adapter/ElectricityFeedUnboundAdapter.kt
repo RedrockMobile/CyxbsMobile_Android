ac762212dee983f63b6886b72d549ef4ae6943e3
Jon <1246634075@qq.com>
2020-03-18 15:37:43 +0800

    [Optimize]优化页面跳转动画

--- src/main/java/com/mredrock/cyxbs/discover/electricity/adapter/ElectricityFeedUnboundAdapter.kt
+++ src/main/java/com/mredrock/cyxbs/discover/electricity/adapter/ElectricityFeedUnboundAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import com.mredrock.cyxbs.common.ui.BaseFeedFragment
-import com.mredrock.cyxbs.common.utils.LogUtils
 import com.mredrock.cyxbs.electricity.R
 import org.jetbrains.anko.layoutInflater
 
