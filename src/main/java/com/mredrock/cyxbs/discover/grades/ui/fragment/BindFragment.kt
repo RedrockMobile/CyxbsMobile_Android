3122f4609d2529a2925618dd013d1bb2c70ea853
roger1245 <1330851383@qq.com>
2020-03-24 08:14:41 +0800

    [Feature] 完成绩点所有功能
    Signed-off-by: roger1245 <1330851383@qq.com>

--- src/main/java/com/mredrock/cyxbs/discover/grades/ui/fragment/BindFragment.kt
+++ src/main/java/com/mredrock/cyxbs/discover/grades/ui/fragment/BindFragment.kt
@@ -1,14 +1,21 @@
 package com.mredrock.cyxbs.discover.grades.ui.fragment
 
+import android.os.Build
 import android.os.Bundle
+import android.transition.Explode
+import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.mredrock.cyxbs.common.utils.extensions.toast
 import com.mredrock.cyxbs.discover.grades.R
 import com.mredrock.cyxbs.discover.grades.ui.viewModel.ContainerViewModel
+import com.mredrock.cyxbs.discover.grades.utils.widget.KeyboardUtil
 import kotlinx.android.synthetic.main.grades_fragment_bind.*
 
 /**
@@ -33,11 +40,50 @@ class BindFragment : Fragment() {
             val ids: String = grades_et_ids.text.toString()
             val password: String = grades_et_password.text.toString()
             if (ids.isNotEmpty() && password.isNotEmpty()) {
-                viewModel.bindIds(ids, password)
+
+                KeyboardUtil.closeKeybord(activity)
+                viewModel.bindIds(ids, password) { bubble() }
             } else {
                 context?.toast("请输入统一认证码和密码哟")
             }
         }
+        viewModel.replaceBindFragmentToGPAFragment.observe(this, Observer {
+            if (it == false) {
+                bubble()
+            }
+        })
+
+        grades_et_password.setOnEditorActionListener { _, actionId, _ ->
+            return@setOnEditorActionListener when (actionId) {
+                EditorInfo.IME_ACTION_SEND -> {
+                    val ids: String = grades_et_ids.text.toString()
+                    val password: String = grades_et_password.text.toString()
+                    if (ids.isNotEmpty() && password.isNotEmpty()) {
+
+                        KeyboardUtil.closeKeybord(activity)
+                        viewModel.bindIds(ids, password) { bubble() }
+                        true
+                    } else {
+                        context?.toast("请输入统一认证码和密码哟")
+                        false
+                    }
+                }
+                else -> false
+            }
+        }
     }
 
+    private fun bubble() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            TransitionManager.beginDelayedTransition(grades_constraint_layout, Explode())
+        }
+        for (i in 0 until grades_constraint_layout.childCount) {
+            val view = grades_constraint_layout[i]
+            view.visibility = when (view.visibility) {
+                View.GONE -> View.VISIBLE
+                View.VISIBLE -> View.GONE
+                else -> View.VISIBLE
+            }
+        }
+    }
 }
--- src/main/java/com/mredrock/cyxbs/discover/grades/ui/viewModel/ContainerViewModel.kt
+++ src/main/java/com/mredrock/cyxbs/discover/grades/ui/viewModel/ContainerViewModel.kt
@@ -7,19 +7,21 @@ package com.mredrock.cyxbs.discover.grades.ui.viewModel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
 import com.mredrock.cyxbs.common.BaseApp
 import com.mredrock.cyxbs.common.network.ApiGenerator
 import com.mredrock.cyxbs.common.network.ApiGeneratorForSign
 import com.mredrock.cyxbs.common.utils.extensions.*
 import com.mredrock.cyxbs.common.viewmodel.BaseViewModel
-import com.mredrock.cyxbs.common.viewmodel.event.SingleLiveEvent
 import com.mredrock.cyxbs.discover.grades.R
 import com.mredrock.cyxbs.discover.grades.bean.Exam
 import com.mredrock.cyxbs.discover.grades.bean.Grade
 import com.mredrock.cyxbs.discover.grades.bean.IdsBean
+import com.mredrock.cyxbs.discover.grades.bean.IdsStatus
 import com.mredrock.cyxbs.discover.grades.bean.analyze.GPAStatus
 import com.mredrock.cyxbs.discover.grades.network.ApiService
 import io.reactivex.Observable
+import retrofit2.HttpException
 
 class ContainerViewModel : BaseViewModel() {
     companion object {
@@ -58,21 +60,58 @@ class ContainerViewModel : BaseViewModel() {
                 }.lifeCycle()
     }
 
-    fun bindIds(idsNum: String, idsPassword: String) {
+    private var isAnimating = false
+
+    fun bindIds(idsNum: String, idsPassword: String, bubble: () -> Unit) {
+        if (isAnimating) {
+            return
+        }
+        isAnimating = true
+        bubble.invoke()
+        val startTime = System.currentTimeMillis()
         apiService.bindIds(IdsBean(idsNum, idsPassword))
+                .doOnNext {
+                    sleepThread(startTime)
+                }
+                .doOnError {
+                    sleepThread(startTime)
+                }
                 .setSchedulers()
-                .safeSubscribeBy {
-                    if (it.status == STATUS_OK) {
-                        BaseApp.context.toast(R.string.grades_bottom_sheet_bind_success)
-                        replaceBindFragmentToGPAFragment.postValue(true)
-                    } else if (it.errorCode == ERROR) {
-                        BaseApp.context.toast(R.string.grades_bottom_sheet_bind_fail)
-                    }
-                }.lifeCycle()
+                .safeSubscribeBy(
+                        onNext = {
+                            BaseApp.context.toast(R.string.grades_bottom_sheet_bind_success)
+                            replaceBindFragmentToGPAFragment.postValue(true)
+                            isAnimating = false
+                        },
+                        onError = {
+                            //密码错误的话,会导致状态码为400，Retrofit无法回调onNext
+                            //详见：https://www.cnblogs.com/fuyaozhishang/p/8607706.html
+                            if (it is HttpException) {
+                                val body = (it).response().errorBody() ?: return@safeSubscribeBy
+                                val data = Gson().fromJson(body.string(), IdsStatus::class.java)
+                                if (data.errorCode == ERROR) {
+                                    replaceBindFragmentToGPAFragment.postValue(false)
+                                    BaseApp.context.toast(R.string.grades_bottom_sheet_bind_fail)
+                                }
+                            } else {
+                                replaceBindFragmentToGPAFragment.postValue(false)
+                                BaseApp.context.toast("绑定ids失败")
+                            }
+                            isAnimating = false
+                        }
+                ).lifeCycle()
 
     }
 
-    val replaceBindFragmentToGPAFragment: SingleLiveEvent<Boolean> = SingleLiveEvent()
+    private fun sleepThread(startTime: Long) {
+        val curTime = System.currentTimeMillis()
+        val waitTime = 2000
+        if (curTime - startTime < waitTime) {
+            Thread.sleep(waitTime - curTime + startTime)
+        }
+    }
+
+    val replaceBindFragmentToGPAFragment: MutableLiveData<Boolean> = MutableLiveData()
 
     private val _analyzeData = MutableLiveData<GPAStatus>()
     val analyzeData: LiveData<GPAStatus>
@@ -86,7 +125,21 @@ class ContainerViewModel : BaseViewModel() {
                             _analyzeData.postValue(it)
                         },
                         onError = {
-                            BaseApp.context.toast("加载绩点失败")
+                            //未绑定的话,会导致状态码为400，Retrofit无法回调onNext
+                            //详见：https://www.cnblogs.com/fuyaozhishang/p/8607706.html
+                            if (it is HttpException) {
+                                val errorBody = it.response().errorBody()?.string() ?: ""
+                                val gpaStatus = Gson().fromJson(errorBody, GPAStatus::class.java)
+                                _analyzeData.postValue(gpaStatus)
+                            } else {
+                                //此时说明是一些其他的错误
+                                val s = "{\"errcode\":\"10010\",\"errmessage\":\"errCode:114514 errMsg: runtime error: index out of range [-1]\"}"
+                                val gpaStatus = Gson().fromJson(s, GPAStatus::class.java)
+                                _analyzeData.postValue(gpaStatus)
+
+                                BaseApp.context.toast("加载绩点失败")
+                            }
+
                         }
                 ).lifeCycle()
     }
