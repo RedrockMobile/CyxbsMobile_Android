20797415a189c6cf7a6603c32bcab67072e38b02
roger1245 <1330851383@qq.com>
2020-03-27 09:29:17 +0800

    [Feature] 修改我的商品页UI
    Signed-off-by: roger1245 <1330851383@qq.com>

--- src/main/java/com/mredrock/cyxbs/mine/page/myproduct/MyProductActivity.kt
+++ src/main/java/com/mredrock/cyxbs/mine/page/myproduct/MyProductActivity.kt
@@ -2,15 +2,31 @@ package com.mredrock.cyxbs.mine.page.myproduct
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.mredrock.cyxbs.mine.util.ui.BaseTabLayoutActivity
+import com.google.android.material.tabs.TabLayout
+import com.mredrock.cyxbs.common.ui.BaseActivity
+import com.mredrock.cyxbs.mine.R
+import com.mredrock.cyxbs.mine.util.ui.TabPagerAdapter
+import kotlinx.android.synthetic.main.mine_activity_tablayout_common.*
 
-class MyProductActivity : BaseTabLayoutActivity() {
+class MyProductActivity(override val isFragmentActivity: Boolean = false) : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        common_toolbar.title = "我的商品"
+        setContentView(R.layout.mine_activity_tablayout_my_product)
+        common_toolbar.initWithSplitLine("", false, R.drawable.mine_ic_arrow_left_my_product)
         val fragmentList = listOf<Fragment>(MyProductFragment(MyProductFragment.UNCLAIMED), MyProductFragment(MyProductFragment.CLAIMED))
         val titleList = listOf("未领取", "已领取")
         init(fragmentList, titleList)
+    }
+
+    fun init(fragmentList: List<Fragment>, titleList: List<String>) {
+
+        //设置viewPager
+        mine_tl_view_pager.adapter = TabPagerAdapter(supportFragmentManager, fragmentList, titleList)
+        mine_tl_view_pager.offscreenPageLimit = fragmentList.size
+
+        mine_tl_tablayout.tabMode = TabLayout.MODE_FIXED
+        mine_tl_tablayout.setupWithViewPager(mine_tl_view_pager)
+
     }
 }
