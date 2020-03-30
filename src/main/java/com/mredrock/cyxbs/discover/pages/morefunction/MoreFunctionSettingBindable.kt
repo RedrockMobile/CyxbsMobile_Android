25512e091a39a1b9927e270e6d4556b6ea37b415
zixuan <931717954@qq.com>
2020-03-30 10:05:02 +0800

    [Bugfix]修正可以选择更多功能至首页的问题。
    Signed-off-by: zixuan <931717954@qq.com>

--- src/main/java/com/mredrock/cyxbs/discover/pages/morefunction/MoreFunctionSettingBindable.kt
+++ src/main/java/com/mredrock/cyxbs/discover/pages/morefunction/MoreFunctionSettingBindable.kt
@@ -9,6 +9,7 @@ import com.mredrock.cyxbs.discover.utils.MoreFunctionProvider
 
 class MoreFunctionSettingBindable : Bindable {
     override fun onBindViewHolder(holder: MoreFunctionRvAdapter.MoreFunctionViewHolder, position: Int, adapter: MoreFunctionRvAdapter) {
+        if(position == MoreFunctionProvider.functions.size - 1)return
         if (!adapter.getChosenList().contains(position)) {
             if (adapter.getChosenList().size > 2) {
                 CyxbsToast.makeText(holder.itemView.context, "只能选择三个哦", Toast.LENGTH_SHORT).show()
@@ -27,15 +28,15 @@ class MoreFunctionSettingBindable : Bindable {
     }
 
     override fun settingClicked(adapter: MoreFunctionRvAdapter, context: Context, imageView: ImageView):Boolean {
-        if (adapter.getChosenList().size == 3) {
+        return if (adapter.getChosenList().size == 3) {
             MoreFunctionProvider.saveHomePageFunctionsToSp(adapter.getChosenList())
             adapter.resetChosenList()
             setMoreFunctionIcon(imageView)
             CyxbsToast.makeText(context, "更新成功", Toast.LENGTH_SHORT).show()
-            return true
+            true
         } else {
             CyxbsToast.makeText(context, "要选择三个哦", Toast.LENGTH_SHORT).show()
-            return false
+            false
         }
     }
 
