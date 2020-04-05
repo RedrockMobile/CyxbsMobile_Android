bdf8825e36c201af0355232c178c04700c7abbc9
Jon <1246634075@qq.com>
2020-04-05 23:51:43 +0800

    [Optimize]调整Main验证加载Fragment的逻辑，清除不必要版本判断

--- src/main/java/com/mredrock/cyxbs/discover/noclass/pages/stuselect/NoClassStuSelectActivity.kt
+++ src/main/java/com/mredrock/cyxbs/discover/noclass/pages/stuselect/NoClassStuSelectActivity.kt
@@ -1,6 +1,5 @@
 package com.mredrock.cyxbs.discover.noclass.pages.stuselect
 
-import android.os.Build
 import android.os.Bundle
 import android.transition.Slide
 import android.view.Gravity
@@ -15,9 +14,7 @@ class NoClassStuSelectActivity : BaseActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
-        }
+        window.requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.discover_noclass_activity_stu_select)
 
@@ -41,13 +38,11 @@ class NoClassStuSelectActivity : BaseActivity() {
         }
     }
 
-    fun initAnimation() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.enterTransition = Slide(Gravity.BOTTOM).apply {
-                duration = 1000
-                excludeTarget(cl_noclass_stu_select, true)
-                addTarget(R.id.ll_stu_select)
-            }
+    private fun initAnimation() {
+        window.enterTransition = Slide(Gravity.BOTTOM).apply {
+            duration = 1000
+            excludeTarget(cl_noclass_stu_select, true)
+            addTarget(R.id.ll_stu_select)
         }
     }
 }
