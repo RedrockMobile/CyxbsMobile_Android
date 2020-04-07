5af92761a1334a390ca28eccb2db22b14246eb38
yyfbe <2861112448@qq.com>
2020-04-07 18:41:13 +0800

    [Optimize]修改对象名

--- src/main/java/com/mredrock/cyxbs/mine/util/Res.kt
+++ src/main/java/com/mredrock/cyxbs/mine/util/Res.kt
@@ -1,7 +1,7 @@
 package com.mredrock.cyxbs.mine.util
 
 import com.mredrock.cyxbs.common.network.ApiGenerator
-import com.mredrock.cyxbs.common.network.ApiGeneratorForSign
+import com.mredrock.cyxbs.common.network.ApiGeneratorForAnother
 import com.mredrock.cyxbs.mine.network.ApiService
 
 /**
@@ -10,4 +10,4 @@ import com.mredrock.cyxbs.mine.network.ApiService
 val apiService: ApiService by lazy { ApiGenerator.getApiService(ApiService::class.java) }
 
 //临时使用，因为后端测试的baseurl与apiService不同，故作区分
-val apiServiceForSign: ApiService by lazy { ApiGeneratorForSign.getApiService(ApiService::class.java) }
+val apiServiceForSign: ApiService by lazy { ApiGeneratorForAnother.getApiService(ApiService::class.java) }
