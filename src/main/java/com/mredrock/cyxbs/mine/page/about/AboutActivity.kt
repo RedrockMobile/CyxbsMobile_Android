c8fe21a6cc42315885a5cc34154470d212a9a830
roger1245 <1330851383@qq.com>
2020-04-08 13:36:45 +0800

    [Feature] 修改分享功能和协议
    Signed-off-by: roger1245 <1330851383@qq.com>

--- src/main/java/com/mredrock/cyxbs/mine/page/about/AboutActivity.kt
+++ src/main/java/com/mredrock/cyxbs/mine/page/about/AboutActivity.kt
@@ -1,35 +1,24 @@
 package com.mredrock.cyxbs.mine.page.about
 
-import android.Manifest
 import android.content.Intent
-import android.graphics.drawable.BitmapDrawable
-import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.view.LayoutInflater
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
-import com.afollestad.materialdialogs.MaterialDialog
 import com.mredrock.cyxbs.common.component.CommonDialogFragment
 import com.mredrock.cyxbs.common.config.APP_WEBSITE
-import com.mredrock.cyxbs.common.config.DIR_PHOTO
 import com.mredrock.cyxbs.common.config.updateFile
 import com.mredrock.cyxbs.common.ui.BaseViewModelActivity
-import com.mredrock.cyxbs.common.utils.extensions.doPermissionAction
-import com.mredrock.cyxbs.common.utils.extensions.saveImage
 import com.mredrock.cyxbs.common.utils.extensions.toast
 import com.mredrock.cyxbs.common.utils.getAppVersionName
 import com.mredrock.cyxbs.common.utils.update.UpdateEvent
 import com.mredrock.cyxbs.common.utils.update.UpdateUtils
 import com.mredrock.cyxbs.mine.R
 import com.mredrock.cyxbs.mine.util.extension.logr
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.mine_activity_about.*
-import kotlinx.android.synthetic.main.mine_dialog_share.view.*
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
+
 
 class AboutActivity(override val isFragmentActivity: Boolean = false,
                     override val viewModelClass: Class<AboutViewModel> = AboutViewModel::class.java)
@@ -98,12 +87,20 @@ class AboutActivity(override val isFragmentActivity: Boolean = false,
     }
 
     private fun clickLegal() {
-        MaterialDialog.Builder(this)
-                .title("使用条款")
-                .content("版权归红岩网校工作站所有,感谢您的使用")
-                .positiveText("确定")
-                .build()
-                .show()
+        val tag = "legal"
+        if (supportFragmentManager.findFragmentByTag(tag) == null) {
+            CommonDialogFragment().apply {
+                initView(
+                        containerRes = R.layout.mine_layout_dialog_feature_intro,
+                        onPositiveClick = { dismiss() },
+                        positiveString = "我知道了",
+                        elseFunction = { rootView ->
+                            rootView.findViewById<TextView>(R.id.mine_about_tv_feature_title).text = "使用条款"
+                            rootView.findViewById<TextView>(R.id.mine_about_tv_feature_content).text = "版权归红岩网校工作站所有,感谢您的使用"
+                        }
+                )
+            }.show(supportFragmentManager, tag)
+        }
     }
 
     private fun setAppVersionName() {
@@ -124,34 +121,10 @@ class AboutActivity(override val isFragmentActivity: Boolean = false,
     }
 
     private fun onShareClick() {
-        val builder = AlertDialog.Builder(this)
-        val view = LayoutInflater.from(this).inflate(R.layout.mine_dialog_share, null, false)
-        val dialog = builder.setView(view).show()
-        val imageView = view.imageView
-        imageView.setOnLongClickListener {
-            doPermissionAction(Manifest.permission.WRITE_EXTERNAL_STORAGE) {
-                reason = "为保存图片需要您的文件写入权限"
-                doAfterGranted {
-                    val drawable = imageView.drawable
-                    if (drawable is BitmapDrawable) {
-                        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
-                        Schedulers.io().scheduleDirect {
-                            this@AboutActivity.saveImage(bitmap, "掌邮二维码")
-                            MediaScannerConnection.scanFile(this@AboutActivity,
-                                    arrayOf(Environment.getExternalStorageDirectory().toString() + DIR_PHOTO),
-                                    arrayOf("image/jpeg"),
-                                    null)
 
-                            runOnUiThread {
-                                toast("图片保存于系统\"$DIR_PHOTO\"文件夹下哦")
-                                dialog.dismiss()
-                            }
-                        }
-                    }
-                }
-            }
-
-            true
-        }
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, "掌上重邮是重邮首款校园生活类App，拥有查课表，签到，邮问等功能，记得分享给好友哦。 https://wx.idsbllp.cn/app/")
+        startActivity(Intent.createChooser(intent, "分享"))
     }
 }
