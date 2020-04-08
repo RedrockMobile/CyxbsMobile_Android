89df82cb6feb3345d091d8272be35621793ae3aa
roger1245 <1330851383@qq.com>
2020-04-08 13:15:07 +0800

    [Feature] 完成版本特性动态下发
    Signed-off-by: roger1245 <1330851383@qq.com>

--- src/main/java/com/mredrock/cyxbs/mine/page/about/AboutActivity.kt
+++ src/main/java/com/mredrock/cyxbs/mine/page/about/AboutActivity.kt
@@ -8,13 +8,15 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.afollestad.materialdialogs.MaterialDialog
+import com.mredrock.cyxbs.common.component.CommonDialogFragment
 import com.mredrock.cyxbs.common.config.APP_WEBSITE
 import com.mredrock.cyxbs.common.config.DIR_PHOTO
 import com.mredrock.cyxbs.common.config.updateFile
-import com.mredrock.cyxbs.common.ui.BaseActivity
+import com.mredrock.cyxbs.common.ui.BaseViewModelActivity
 import com.mredrock.cyxbs.common.utils.extensions.doPermissionAction
 import com.mredrock.cyxbs.common.utils.extensions.saveImage
 import com.mredrock.cyxbs.common.utils.extensions.toast
@@ -22,13 +24,16 @@ import com.mredrock.cyxbs.common.utils.getAppVersionName
 import com.mredrock.cyxbs.common.utils.update.UpdateEvent
 import com.mredrock.cyxbs.common.utils.update.UpdateUtils
 import com.mredrock.cyxbs.mine.R
+import com.mredrock.cyxbs.mine.util.extension.logr
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.mine_activity_about.*
 import kotlinx.android.synthetic.main.mine_dialog_share.view.*
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 
-class AboutActivity(override val isFragmentActivity: Boolean = false) : BaseActivity() {
+class AboutActivity(override val isFragmentActivity: Boolean = false,
+                    override val viewModelClass: Class<AboutViewModel> = AboutViewModel::class.java)
+    : BaseViewModelActivity<AboutViewModel>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +46,12 @@ class AboutActivity(override val isFragmentActivity: Boolean = false) : BaseActi
                     R.drawable.mine_ic_arrow_left)
             setTitleLocationAtLeft(true)
         }
+        getAppVersionName(this@AboutActivity)?.let {
+            logr(it)
+            val name = "zscy-feature-intro-${it}"
+            viewModel.getFeatureIntroduction(name)
+        }
+
 
         setAppVersionName()
 
@@ -49,6 +60,30 @@ class AboutActivity(override val isFragmentActivity: Boolean = false) : BaseActi
         mine_about_legal.setOnClickListener { clickLegal() }
         mine_about_rl_update.setOnClickListener { clickUpdate() }
         mine_about_rl_share.setOnClickListener { onShareClick() }
+
+        mine_about_rl_function.setOnClickListener { clickFeatureIntroduction() }
+    }
+
+    private fun clickFeatureIntroduction() {
+        val tag = "feature"
+        if (supportFragmentManager.findFragmentByTag(tag) == null) {
+            CommonDialogFragment().apply {
+                initView(
+                        containerRes = R.layout.mine_layout_dialog_feature_intro,
+                        onPositiveClick = { dismiss() },
+                        positiveString = "我知道了",
+                        elseFunction = { rootView ->
+                            viewModel.featureIntro.value?.let {
+                                if (it.textList.isNotEmpty()) {
+                                    val feature = it.textList.last()
+                                    rootView.findViewById<TextView>(R.id.mine_about_tv_feature_title).text = feature.title
+                                    rootView.findViewById<TextView>(R.id.mine_about_tv_feature_content).text = feature.content
+                                }
+                            }
+                        }
+                )
+            }.show(supportFragmentManager, tag)
+        }
     }
 
     private fun clickUpdate() {
---
+++ src/main/java/com/mredrock/cyxbs/mine/page/about/AboutViewModel.kt
@@ -0,0 +1,36 @@
+package com.mredrock.cyxbs.mine.page.about
+
+import androidx.lifecycle.MutableLiveData
+import com.mredrock.cyxbs.common.BaseApp
+import com.mredrock.cyxbs.common.network.ApiGeneratorForAnother
+import com.mredrock.cyxbs.common.network.CommonApiService
+import com.mredrock.cyxbs.common.utils.down.bean.DownMessage
+import com.mredrock.cyxbs.common.utils.down.params.DownMessageParams
+import com.mredrock.cyxbs.common.utils.extensions.safeSubscribeBy
+import com.mredrock.cyxbs.common.utils.extensions.toast
+import com.mredrock.cyxbs.common.viewmodel.BaseViewModel
+import com.mredrock.cyxbs.mine.util.extension.normalWrapper
+
+/**
+ * Created by roger on 2020/4/8
+ */
+class AboutViewModel : BaseViewModel() {
+
+    var featureIntro : MutableLiveData<DownMessage> = MutableLiveData()
+    private set
+
+
+    fun getFeatureIntroduction(name: String) {
+        ApiGeneratorForAnother.getCommonApiService(CommonApiService::class.java)
+                .getDownMessage(DownMessageParams(name))
+                .normalWrapper(this)
+                .safeSubscribeBy(
+                        onNext = {
+                            featureIntro.postValue(it)
+                        },
+                        onError = {
+                            BaseApp.context.toast("获取功能介绍失败")
+                        }
+                ).lifeCycle()
+    }
+}
