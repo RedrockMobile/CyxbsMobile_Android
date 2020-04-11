8ca699e1626bc07ca0d2ee6cd57b80fcd3b41027
Jon <1246634075@qq.com>
2020-04-11 15:34:26 +0800

    [BugFix]解决内存不足重新启动之后导致fragment多次加载

--- src/main/java/com/mredrock/cyxbs/main/utils/AbnormalUtil.kt
+++ src/main/java/com/mredrock/cyxbs/main/utils/AbnormalUtil.kt
@@ -13,7 +13,13 @@ import com.mredrock.cyxbs.main.ui.MainActivity
 
 //根据ARouterPath来查询是否已经加载当前Fragment，以此来增强app在Activity异常重启时的稳定性
 fun List<Fragment>.entryContains(aRouterPath: String, activity: MainActivity): Fragment? {
-    val list = filter { it::class.java.name === activity.mainPageLoadedFragmentClassList[aRouterPath] }
+    val list = filter {
+        //这里这个等号有个坑，记录一下
+        //Activity由于内存原因被系统杀死之后，Bundle里面是有数据的，因为app推到后台的时候就进行了保存
+        //但是不会调用onRestoreInstanceState,此时,由于我们这里是使用全类名来保证加载的
+        //这里的这里的字符串内存地址不是一样的，也就是说字符串常量池中有这两个字符串
+        it::class.java.name == activity.mainPageLoadedFragmentClassList[aRouterPath]
+    }
     return if (list.isEmpty()) null else list[0]
 }
 
