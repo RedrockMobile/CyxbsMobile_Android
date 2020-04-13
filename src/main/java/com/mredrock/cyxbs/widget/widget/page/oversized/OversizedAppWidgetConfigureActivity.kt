dbd271dd055d9b5b15b5fbfc5c246586ce0c676e
Jon <1246634075@qq.com>
2020-04-13 11:32:42 +0800

    [Optimize]整理代码

--- src/main/java/com/mredrock/cyxbs/widget/widget/page/oversized/OversizedAppWidgetConfigureActivity.kt
+++ src/main/java/com/mredrock/cyxbs/widget/widget/page/oversized/OversizedAppWidgetConfigureActivity.kt
@@ -12,24 +12,12 @@ import android.view.WindowManager
 import com.mredrock.cyxbs.widget.R
 import com.mredrock.cyxbs.widget.widget.oversize.updateAppWidget
 
-/**
- * The configuration screen for the [OversizedAppWidget] AppWidget.
- */
 class OversizedAppWidgetConfigureActivity : Activity() {
     private var appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
-//    private lateinit var appWidgetText: EditText
     private var onClickListener = View.OnClickListener {
         val context = this@OversizedAppWidgetConfigureActivity
-
-        // When the button is clicked, store the string locally
-//        val widgetText = appWidgetText.text.toString()
-//        saveTitlePref(context, appWidgetId, widgetText)
-
-        // It is the responsibility of the configuration activity to update the app widget
         val appWidgetManager = AppWidgetManager.getInstance(context)
         updateAppWidget(context, appWidgetManager, appWidgetId)
-
-        // Make sure we pass back the original appWidgetId
         val resultValue = Intent()
         resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
         setResult(RESULT_OK, resultValue)
@@ -38,54 +26,27 @@ class OversizedAppWidgetConfigureActivity : Activity() {
 
     public override fun onCreate(icicle: Bundle?) {
         super.onCreate(icicle)
-
-        // Set the result to CANCELED.  This will cause the widget host to cancel
-        // out of the widget placement if the user presses the back button.
         setResult(RESULT_CANCELED)
-
         setContentView(R.layout.widget_oversized_app_widget_configure)
         window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         window.attributes.gravity = Gravity.BOTTOM
-//        appWidgetText = findViewById<View>(R.id.appwidget_text) as EditText
         findViewById<View>(R.id.add_button).setOnClickListener(onClickListener)
-
-        // Find the widget id from the intent.
         val intent = intent
         val extras = intent.extras
         if (extras != null) {
             appWidgetId = extras.getInt(
                     AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID)
         }
-
-        // If this activity was started with an intent without an app widget ID, finish with an error.
         if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
             finish()
             return
         }
-
-//        appWidgetText.setText(loadTitlePref(this@OversizedAppWidgetConfigureActivity, appWidgetId))
     }
 
 }
 
 private const val PREFS_NAME = "com.mredrock.cyxbs.widget.widget.oversize.OversizedAppWidget"
 private const val PREF_PREFIX_KEY = "appwidget_"
-
-// Write the prefix to the SharedPreferences object for this widget
-internal fun saveTitlePref(context: Context, appWidgetId: Int, text: String) {
-    val prefs = context.getSharedPreferences(PREFS_NAME, 0).edit()
-    prefs.putString(PREF_PREFIX_KEY + appWidgetId, text)
-    prefs.apply()
-}
-
-// Read the prefix from the SharedPreferences object for this widget.
-// If there is no preference saved, get the default from a resource
-internal fun loadTitlePref(context: Context, appWidgetId: Int): String {
-    val prefs = context.getSharedPreferences(PREFS_NAME, 0)
-    val titleValue = prefs.getString(PREF_PREFIX_KEY + appWidgetId, null)
-    return titleValue ?: context.getString(R.string.appwidget_text)
-}
-
 internal fun deleteTitlePref(context: Context, appWidgetId: Int) {
     val prefs = context.getSharedPreferences(PREFS_NAME, 0).edit()
     prefs.remove(PREF_PREFIX_KEY + appWidgetId)
