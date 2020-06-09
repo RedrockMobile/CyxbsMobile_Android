86b0da435e844d504328531dc595186bcc38c15e
Jovines <1246634075@qq.com>
2020-06-09 20:56:40 +0800

    [Optimize]lib用线程池替换IntentService

--- src/main/java/com/mredrock/cyxbs/App.kt
+++ src/main/java/com/mredrock/cyxbs/App.kt
@@ -1,8 +1,9 @@
 package com.mredrock.cyxbs
 
-import android.content.Context
 import com.mredrock.cyxbs.common.BaseApp
 import com.taobao.sophix.SophixManager
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
 
 /**
  * Created By jay68 on 2018/8/8.
@@ -11,7 +12,10 @@ class App : BaseApp() {
 
     override fun onCreate() {
         super.onCreate()
-        AppInitService.init(this)
+        //若以后还会有这种非必须在application启动时初始化的第三方SDK请写在InitTask中然后添加到这里的just里面
+        Observable.just(
+                initBugly(context)
+        ).subscribeOn(Schedulers.computation()).doOnNext { it() }.subscribe()
         SophixManager.getInstance().queryAndLoadNewPatch()
     }
 }
