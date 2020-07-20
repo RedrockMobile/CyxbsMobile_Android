e9b327d2623a02e5d6a56625b037d73efac94fc1
yyfbe <2861112448@qq.com>
2020-07-20 16:06:26 +0800

    [Optimize]规范代码

--- src/main/java/com/mredrock/cyxbs/discover/schoolcar/MapStyleHelper.kt
+++ src/main/java/com/mredrock/cyxbs/discover/schoolcar/MapStyleHelper.kt
@@ -2,6 +2,7 @@ package com.mredrock.cyxbs.discover.schoolcar
 
 import android.content.Context
 import com.mredrock.cyxbs.common.config.END_POINT_REDROCK_VERSION_TWO
+import com.mredrock.cyxbs.common.config.SCHOOL_CAR_MAP_RES
 import com.mredrock.cyxbs.common.network.ApiGenerator
 import com.mredrock.cyxbs.common.utils.LogUtils
 import com.mredrock.cyxbs.common.utils.extensions.defaultSharedPreferences
@@ -24,7 +25,7 @@ class MapStyleHelper(val context: Context) {
 
     fun saveMapStyle(callback: () -> Unit) {
         checkFile()
-        ApiGenerator.registerNetSettings(0, retrofitConfig = { builder: Retrofit.Builder ->
+        ApiGenerator.registerNetSettings(SCHOOL_CAR_MAP_RES, retrofitConfig = { builder: Retrofit.Builder ->
             builder.baseUrl(END_POINT_REDROCK_VERSION_TWO)
         },tokenNeeded = true)
         val apiService = ApiGenerator.getApiService(0, MapService::class.java)
