61e61152cd56b8f08ca148366258c305b96a0ace
yyfbe <2861112448@qq.com>
2020-08-25 13:01:56 +0800

    [optimize]更换闪屏页接口

--- src/main/java/com/mredrock/cyxbs/main/network/ApiService.kt
+++ src/main/java/com/mredrock/cyxbs/main/network/ApiService.kt
@@ -3,12 +3,12 @@ package com.mredrock.cyxbs.main.network
 import com.mredrock.cyxbs.common.bean.RedrockApiWrapper
 import com.mredrock.cyxbs.main.bean.StartPage
 import io.reactivex.Observable
-import retrofit2.http.POST
+import retrofit2.http.GET
 
 /**
  * Created By jay68 on 2018/8/10.
  */
 interface ApiService {
-    @POST("/app/index.php/Home/Photo/showPicture")
+    @GET("/wxapi/magipoke-text/start/photo")
     fun getStartPage(): Observable<RedrockApiWrapper<List<StartPage>>>
 }
--- src/main/java/com/mredrock/cyxbs/main/ui/MainActivity.kt
+++ src/main/java/com/mredrock/cyxbs/main/ui/MainActivity.kt
@@ -212,6 +212,11 @@ class MainActivity : BaseViewModelActivity<MainViewModel>(), EventBusLifecycleSu
     private fun checkSplash() {
         //判断是否下载了Splash图，下载了就直接显示
         if (isDownloadSplash(this@MainActivity)) {
+            if (!ServiceManager.getService(IAccountService::class.java).getVerifyService().isLogin() && !ServiceManager.getService(IAccountService::class.java).getVerifyService().isTouristMode()) {
+                //表示，已经下载了，但是用户主动退出登录
+                //这里判断的依据是防止onCreate()加载了闪屏页fragment，onStart()跳转到登录Activity，MainActivity被销毁，fragment没有activity
+                return
+            }
             main_activity_splash_viewStub.onTouch { _, _ -> }//防止穿透点击
             viewModel.splashVisibility.set(View.VISIBLE)//显示闪屏页容器
             supportFragmentManager.beginTransaction().replace(R.id.main_activity_splash_viewStub, SplashFragment()).commit()
