be4ec72834fe6f9bcf01dcf76403907c68338466
yyfbe <2861112448@qq.com>
2020-09-01 15:17:03 +0800

    [optimize]通过spi提供fragment

--- src/main/java/com/mredrock/cyxbs/volunteer/DiscoverVolunteerFeedViewModel.kt
+++ src/main/java/com/mredrock/cyxbs/volunteer/DiscoverVolunteerFeedViewModel.kt
@@ -1,6 +1,5 @@
 package com.mredrock.cyxbs.volunteer
 
-import com.mredrock.cyxbs.common.config.DISCOVERY_VOLUNTEER
 import com.mredrock.cyxbs.common.network.ApiGenerator
 import com.mredrock.cyxbs.common.utils.LogUtils
 import com.mredrock.cyxbs.common.utils.extensions.safeSubscribeBy
@@ -9,12 +8,11 @@ import com.mredrock.cyxbs.common.viewmodel.BaseViewModel
 import com.mredrock.cyxbs.common.viewmodel.event.SingleLiveEvent
 import com.mredrock.cyxbs.volunteer.bean.VolunteerTime
 import com.mredrock.cyxbs.volunteer.network.ApiService
-import java.util.concurrent.TimeUnit
 
 class DiscoverVolunteerFeedViewModel : BaseViewModel() {
     val volunteerData = SingleLiveEvent<VolunteerTime>()
     private val apiService: ApiService by lazy {
-        ApiGenerator.getApiService(DISCOVERY_VOLUNTEER, ApiService::class.java)
+        ApiGenerator.getApiService(ApiService::class.java)
     }
 
     fun loadVolunteerTime(uid: String) {
