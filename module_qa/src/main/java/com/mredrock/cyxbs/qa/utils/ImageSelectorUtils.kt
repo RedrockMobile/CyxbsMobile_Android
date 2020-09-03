602d234b5e6602a934dd51ee84fa727fc7693471
yyfbe <2861112448@qq.com>
2020-09-03 11:32:07 +0800

    [optimize]增加修改后的图片选择器aar

--- module_qa/src/main/java/com/mredrock/cyxbs/qa/utils/ImageSelectorUtils.kt
+++ module_qa/src/main/java/com/mredrock/cyxbs/qa/utils/ImageSelectorUtils.kt
@@ -1,12 +1,12 @@
 package com.mredrock.cyxbs.qa.utils
 
 import android.Manifest
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import com.mredrock.cyxbs.common.utils.extensions.doPermissionAction
 import com.mredrock.cyxbs.common.utils.extensions.longToast
 import com.mredrock.cyxbs.qa.R
-import top.limuyang2.photolibrary.activity.LPhotoPickerActivity
-import top.limuyang2.photolibrary.engine.LGlideEngine
+import top.limuyang2.photolibrary.LPhotoHelper
 import top.limuyang2.photolibrary.util.LPPImageType
 
 /**
@@ -17,18 +17,22 @@ const val CHOOSE_PHOTO_REQUEST = 0x1024
 
 fun AppCompatActivity.selectImageFromAlbum(maxCount: Int, selected: ArrayList<String>?) {
     doPermissionAction(Manifest.permission.WRITE_EXTERNAL_STORAGE) {
-        doAfterGranted {
-            val intent = LPhotoPickerActivity.IntentBuilder(this@selectImageFromAlbum)
-                    .maxChooseCount(maxCount)
-                    .columnsNumber(4)
-                    .imageType(LPPImageType.ofAll())
-                    .imageEngine(LGlideEngine())
-                    .selectedPhotos(selected)
-                    .theme(R.style.qa_LPhotoTheme)
-                    .build()
-
-            startActivityForResult(intent, CHOOSE_PHOTO_REQUEST)
+        val selectedList: ArrayList<Uri>? = if (selected.isNullOrEmpty()) {
+            null
+        } else {
+            ArrayList<Uri>(selected?.map { Uri.parse(it) })
         }
+        LPhotoHelper.Builder()
+                .maxChooseCount(maxCount) //最多选几个
+                .columnsNumber(3) //每行显示几列图片
+                .imageType(LPPImageType.ofAll()) // 文件类型
+                .pauseOnScroll(false) // 是否滑动暂停加载图片显示
+                .isSingleChoose(false) // 是否是单选
+                .isOpenLastAlbum(false) // 是否直接打开最后一次选择的相册
+                .selectedPhotos(selectedList)
+                .theme(R.style.common_LPhotoTheme)
+                .build()
+                .start(this@selectImageFromAlbum, CHOOSE_PHOTO_REQUEST)
 
         doAfterRefused {
             longToast("访问相册失败，原因：未授权")
