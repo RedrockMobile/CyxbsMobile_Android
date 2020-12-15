2822b26aba6dc23d9d4ed7b6b0bb0df01bcca8e8
sandyz987 <1409618590@qq.com>
2020-12-15 15:35:52 +0800

    [feature]显示回复高亮

--- module_qa/src/main/java/com/mredrock/cyxbs/qa/pages/dynamic/ui/adapter/ReplyListAdapter.kt
+++ module_qa/src/main/java/com/mredrock/cyxbs/qa/pages/dynamic/ui/adapter/ReplyListAdapter.kt
@@ -1,6 +1,10 @@
 package com.mredrock.cyxbs.qa.pages.dynamic.ui.adapter
 
+import android.os.Build
+import android.text.Html
+import android.text.Html.FROM_HTML_MODE_COMPACT
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import com.mredrock.cyxbs.common.utils.extensions.setAvatarImageFromUrl
 import com.mredrock.cyxbs.qa.R
 import com.mredrock.cyxbs.qa.beannew.Comment
@@ -20,11 +24,18 @@ class ReplyListAdapter() : BaseRvAdapter<Comment>() {
 
 
     class ReplyViewHolder(parent: ViewGroup) : BaseViewHolder<Comment>(parent, R.layout.qa_recycler_item_dynamic_reply_inner) {
+        @RequiresApi(Build.VERSION_CODES.N)
         override fun refresh(data: Comment?) {
             data?: return
             itemView.apply {
                 qa_tv_reply_inner_nickname.text = data.nickName
-                qa_tv_reply_inner_content.text = data.content
+                val test = "张三"
+                if (test.isNullOrEmpty()) {
+                    qa_tv_reply_inner_content.text = data.content
+                } else {
+                    val s = "回复<font color=\"#0000FF\"> @${test}</font> : ${data.content}"
+                    qa_tv_reply_inner_content.text = Html.fromHtml(s, FROM_HTML_MODE_COMPACT)
+                }
                 qa_tv_reply_inner_praise_count.text = data.praiseCount.toString()
                 qa_iv_reply_inner_praise_count_image.isChecked = data.isPraised
                 qa_iv_reply_inner_avatar.setAvatarImageFromUrl(data.avatar)
