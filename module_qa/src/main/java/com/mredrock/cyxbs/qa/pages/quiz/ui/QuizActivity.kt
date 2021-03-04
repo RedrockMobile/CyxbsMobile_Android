ea9319cf24a71490f3af510e4344e6cd031fa3a0
RayleighZ <57106748+RayleighZ@users.noreply.github.com>
2021-03-04 19:17:50 +0800

    [optimize]删除无用导包

--- module_qa/src/main/java/com/mredrock/cyxbs/qa/pages/quiz/ui/QuizActivity.kt
+++ module_qa/src/main/java/com/mredrock/cyxbs/qa/pages/quiz/ui/QuizActivity.kt
@@ -8,14 +8,12 @@ import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
-import android.text.TextUtils
 import android.text.TextWatcher
 import android.util.Base64
 import android.view.KeyEvent
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.alibaba.android.arouter.facade.annotation.Route
