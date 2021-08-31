5e02574275896440f8df3961d98a7cc13527dfeb
此处填你的名字 <1356542512@qq.com>
2021-08-31 19:42:18 +0800

    [fix]room schema

--- module_othercourse/src/main/java/com/mredrock/cyxbs/discover/othercourse/room/HistoryDatabase.kt
+++ module_othercourse/src/main/java/com/mredrock/cyxbs/discover/othercourse/room/HistoryDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [History::class], version = 2)
+@Database(entities = [History::class], version = 2,exportSchema = false)
 abstract class HistoryDatabase : RoomDatabase() {
     abstract fun getHistoryDao(): HistoryDao
 
