90a68a4e2005860330181c40f234664e495d12e8
False-Mask <2623036785@qq.com>
2021-09-01 18:26:40 +0800

    [feature] 加入缺省页

--- module_mine/src/main/java/com/mredrock/cyxbs/mine/page/feedback/adapter/BindingAdapters.kt
+++ module_mine/src/main/java/com/mredrock/cyxbs/mine/page/feedback/adapter/BindingAdapters.kt
@@ -1,9 +1,13 @@
 package com.mredrock.cyxbs.mine.page.feedback.adapter
 
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
@@ -24,7 +28,7 @@ object BindingAdapters {
         view: View?,
         state: Boolean?,
         backgroundFalse: Drawable?,
-        backgroundTrue: Drawable?
+        backgroundTrue: Drawable?,
     ) {
         if (state == null) return
         if (state) {
@@ -39,7 +43,7 @@ object BindingAdapters {
     fun netImage(
         imageView: ImageView, url: String?,
         placeholder: Drawable?,
-        error: Drawable?
+        error: Drawable?,
     ) {
         url ?: return
         if (!url.matches(Regex("http.+"))) return
@@ -50,11 +54,29 @@ object BindingAdapters {
     @BindingAdapter("localImage")
     fun localImage(
         imageView: ImageView?,
-        imageUri: Uri?
+        imageUri: Uri?,
     ) {
         imageView ?: return
         imageUri ?: return
         Glide.with(imageView).load(imageUri).into(imageView)
+    }
+
+    @JvmStatic
+    @BindingAdapter("stateColor", "colorFalse", "colorTrue", requireAll = true)
+    fun stateColor(
+        textView: TextView?,
+        stateColor: Boolean?,
+        @ColorInt colorFalse: Int?,
+        @ColorInt colorTrue: Int?,
+    ) {
+        textView ?: return
+        stateColor ?: return
+        if (stateColor == false){
+            colorFalse?.let { textView.setTextColor(it) }
+        }else{
+            colorTrue?.let { textView.setTextColor(it) }
+        }
+
     }
 
 }
