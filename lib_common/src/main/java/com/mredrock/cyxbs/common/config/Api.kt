4e6741cb30b2ba516f44885b1982418116c03066
RayleighZ <3226139433@qq.com>
2021-09-02 10:04:02 +0800

    [release]版本号修改 基础域名和下发域名修改

--- lib_common/src/main/java/com/mredrock/cyxbs/common/config/Api.kt
+++ lib_common/src/main/java/com/mredrock/cyxbs/common/config/Api.kt
@@ -6,13 +6,13 @@ import com.mredrock.cyxbs.common.BuildConfig
  */
 
 //6.1.0版本之后后端环境更新，baseUrl分为测试环境和线上环境
-const val END_POINT_REDROCK_DEV = "https://be-dev.redrock.team"//测试环境url
-const val END_POINT_REDROCK_PROD = "https://be-prod.redrock.team"//线上环境url
+const val END_POINT_REDROCK_DEV = "https://be-dev.redrock.cqupt.edu.cn"//测试环境url
+const val END_POINT_REDROCK_PROD = "https://be-prod.redrock.cqupt.edu.cn"//线上环境url
 const val BASE_NORMAL_IMG_URL = "$END_POINT_REDROCK_PROD/app/Public/photo/"
 
 const val BASE_THUMBNAIL_IMG_URL = BASE_NORMAL_IMG_URL + "thumbnail/"
 
-const val BASE_NORMAL_BACKUP_GET = "https://be-prod.tx.redrock.team/cloud-manager/check"
+const val BASE_NORMAL_BACKUP_GET = "https://be-prod.redrock.team"
 
 //获取baseUrl的方法
 //TODO: 暂时没有测试环境，就都返回了线上环境
