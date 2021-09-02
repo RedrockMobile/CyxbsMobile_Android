909db1043ed618f040c2fe4d3b9edeb2bbf3b260
aefottt <aefottt@outlook.com>
2021-09-02 19:10:51 +0800

    [fix] 修复域名下发地址，增加域名空判断

--- lib_common/src/main/java/com/mredrock/cyxbs/common/network/ApiGenerator.kt
+++ lib_common/src/main/java/com/mredrock/cyxbs/common/network/ApiGenerator.kt
@@ -276,7 +276,7 @@ object ApiGenerator {
     class BackupInterceptor : Interceptor {
 
         private var useBackupUrl: Boolean = false
-        private var backupUrl: String = ""
+        private var backupUrl: String = getBaseUrlWithoutHttps()
 
         override fun intercept(chain: Interceptor.Chain): Response {
 
@@ -327,14 +327,19 @@ object ApiGenerator {
             val json = call.execute().body?.string()
             return try {
                 val backupUrlStatus = Gson().fromJson<RedrockApiWrapper<BackupUrlStatus>>(json, object : TypeToken<RedrockApiWrapper<BackupUrlStatus>>() {}.type)
-                backupUrlStatus.data.baseUrl
+                if (backupUrlStatus.data.baseUrl.isNullOrEmpty()) {
+                    LogUtils.d("LJXiao"," 111111")
+                    getBaseUrlWithoutHttps()
+                }else{
+                    backupUrlStatus.data.baseUrl
+                }
             } catch (e: Exception) {
                 e.printStackTrace()
-                ""
+                getBaseUrlWithoutHttps()
             }
         }
 
 
     }
-
+    private fun getBaseUrlWithoutHttps() = getBaseUrl().subSequence(8, getBaseUrl().length).toString()
 }
