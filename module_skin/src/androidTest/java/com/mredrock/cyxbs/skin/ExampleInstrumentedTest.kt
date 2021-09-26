26018858e02953d7c574e99493b369424e913c0c
LinT0T <737138090@qq.com>
2021-09-26 18:14:21 +0800

    [optimize]:删除单元测试

---
+++ module_skin/src/androidTest/java/com/mredrock/cyxbs/skin/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package com.mredrock.cyxbs.skin
+
+//import android.content.Context
+//
+////
+///**
+// * Instrumented test, which will execute on an Android device.
+// *
+// * @see [Testing documentation](http://d.android.com/tools/testing)
+// */
+//@org.junit.runner.RunWith(AndroidJUnit4::class)
+//class ExampleInstrumentedTest {
+//    @org.junit.Test
+//    fun useAppContext() {
+//        // Context of the app under test.
+//        val appContext: Context = androidx.test.InstrumentationRegistry.getTargetContext()
+//        org.junit.Assert.assertEquals("com.mredrock.cyxbs.common.test", appContext.packageName)
+//    }
+//}
