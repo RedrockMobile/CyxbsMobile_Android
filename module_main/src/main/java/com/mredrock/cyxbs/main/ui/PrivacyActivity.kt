e463175b99c7d923a665ae78a79d65e79279925d
wzt <2238091763@qq.com>
2021-10-07 14:02:38 +0800

    [feature]前端页面用户隐私页面黑夜适配

--- module_main/src/main/java/com/mredrock/cyxbs/main/ui/PrivacyActivity.kt
+++ module_main/src/main/java/com/mredrock/cyxbs/main/ui/PrivacyActivity.kt
@@ -8,9 +8,10 @@ import com.mredrock.cyxbs.main.R
 import kotlinx.android.synthetic.main.main_activity_privacy.*
 
 class PrivacyActivity : BaseActivity() {
-    companion object{
+    companion object {
         const val USER_AGREEMENT_URL = "https://fe-prod.redrock.cqupt.edu.cn/privacy-policy"
     }
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +28,17 @@ class PrivacyActivity : BaseActivity() {
             mediaPlaybackRequiresUserGesture = false
         }
         main_wv_privacy.loadUrl(USER_AGREEMENT_URL)
-        
+
         main_user_privacy_back.setOnClickListener {
             finish()
+        }
+
+        /**
+         * 传值给前端让前端知晓用户是否处于黑夜模式从而页面黑夜适配
+         */
+        fun sendInfoToJs() {
+            val darkModeInfo = applicationContext.resources.configuration.uiMode == 0x21
+            main_wv_privacy.loadUrl("javascript:showInfoFromJava('$darkModeInfo')")
         }
     }
 }
