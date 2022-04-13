b59b60cd645f2b96662382d2beb42bf90c913ce6
SnowOwlet <2457968197@qq.com>
2022-04-13 20:38:14 +0800

    [release] 恢复校车，新增邮问，个人中心，反馈中心

--- build_logic/src/main/kotlin/versions/AGP.kt
+++ build_logic/src/main/kotlin/versions/AGP.kt
@@ -8,8 +8,8 @@ object AGP {
 
 
     const val releaseApplicationId = "com.mredrock.cyxbs"
-    const val releaseVersionCode = 75 // 线上75，开发76
-    const val releaseVersionName = "6.4.2" // 线上6.4.2，开发6.4.3
+    const val releaseVersionCode = 77 // 线上75，开发77
+    const val releaseVersionName = "6.4.4" // 线上6.4.2，开发6.4.4
 
     val releaseAbiFilters = listOf("arm64-v8a")
 }
