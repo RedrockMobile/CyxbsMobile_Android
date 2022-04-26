0f23a4a13cdf09658344fbaec0d188b3310e66bf
False-Mask <2623036785@qq.com>
2022-04-26 18:42:57 +0800

    [fix] 解决在非main进程使用workManager导致进程崩溃的问题。 :( 非main进程指的就是channel了...目前也只有友盟推送这一个额外的进程

--- module_app/src/main/java/com/mredrock/cyxbs/App.kt
+++ module_app/src/main/java/com/mredrock/cyxbs/App.kt
@@ -19,27 +19,33 @@ class App : BaseApp(), SdkManager {
 
     override fun onCreate() {
         super.onCreate()
-        sdks()
-        tasks()
-    }
-
-    private fun tasks() {
-        taskLoader.forEach {
-            it.work()
+        //由于android每开辟进程都会访问application的生命周期方法,所以为了保证sdk初始化无措，最好对其进行过滤。
+        //因为有些sdk的初始化不是幂等的，即多次初始化会导致进程的crash。这样就会导致一些未知的问题。
+        //所以解决方案就是对当前进程进程判断，只在main进程初始化sdk，其余进程默认不进行sdk的初始化。
+        // (不排除某些sdk需要，比如友盟推送就需要在新开辟的:channel进行进行初始化)
+        if (isMainProcess()){
+            onMainProcess()
+        }else {
+            onOtherProcess()
         }
     }
 
-    private fun sdks() {
-        //如果是在主进程，遍历所有的SdkService实现类的onMainProcess
-        sdkLoader.takeIf {
-            isMainProcess()
-        }?.forEach {
-            it.onMainProcess(this)
-        } ?:
-        //如果是在子进程，通过isSdkProcess匹配对应SdkService实现类，最后调用onSdkProcess方法。
+    private fun onOtherProcess() {
+        //在sdk所在的进程初始化
         sdkLoader.firstOrNull {
             it.isSdkProcess(this)
         }?.onSdkProcess(this)
+    }
+
+    private fun onMainProcess() {
+        //初始化sdk
+        sdkLoader.forEach {
+            it.onMainProcess(this)
+        }
+        //通过workManager执行一些任务
+        taskLoader.forEach {
+            it.work()
+        }
     }
 
     override val application: Application
--- module_app/src/main/java/com/mredrock/cyxbs/tasks/WidgetRefresher.kt
+++ module_app/src/main/java/com/mredrock/cyxbs/tasks/WidgetRefresher.kt
@@ -1,8 +1,15 @@
 package com.mredrock.cyxbs.tasks
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.mredrock.cyxbs.R
 import androidx.work.*
 import com.google.auto.service.AutoService
 import com.mredrock.cyxbs.common.BaseApp
@@ -21,11 +28,11 @@ class WidgetRefresher : TaskService {
         WorkManager.getInstance(BaseApp.appContext)
             .enqueue(
                 listOf(
-                    OneTimeWorkRequestBuilder<WidgetRefreshWork>().setExpedited(
-                        OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST
-                    ).build(),
-                    PeriodicWorkRequestBuilder<WidgetRefreshWork>(15.minutes.toJavaDuration())
+                    //先进行一次加急任务
+                    OneTimeWorkRequestBuilder<WidgetRefreshWork>().setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
                         .build(),
+                    //加入一个15分钟的周期任务,虽然用处不大,说不定有那么一点用不是。
+                    PeriodicWorkRequestBuilder<WidgetRefreshWork>(15.minutes.toJavaDuration()).build(),
                 )
             )
 
@@ -33,8 +40,13 @@ class WidgetRefresher : TaskService {
 }
 
 class WidgetRefreshWork(private val context: Context, workerParams: WorkerParameters) :
-    Worker(context, workerParams) {
-    override fun doWork(): Result {
+    CoroutineWorker(context, workerParams) {
+
+    val WIDGET_UPDATA_NOTIFICATION_ID = 10
+    val CHANNLE_ID = "widget_update"
+    val CHANNEL_NAME = "widget_update_channel"
+
+    override suspend fun doWork(): Result {
         val ctx = context
         widgetList.forEach { (action, pkg) ->
             ctx.sendBroadcast(Intent(action).apply {
@@ -44,11 +56,34 @@ class WidgetRefreshWork(private val context: Context, workerParams: WorkerParame
         return Result.success()
     }
 
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+
+        return ForegroundInfo(WIDGET_UPDATA_NOTIFICATION_ID, createNotification())
+    }
+
+    private fun createNotification(): Notification {
+
+        val manager = NotificationManagerCompat.from(context)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNLE_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            manager.createNotificationChannel(notificationChannel)
+        }
+
+        return NotificationCompat.Builder(context, CHANNLE_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .build()
+    }
+
+
 }
 
 const val littleWidgetPkg = "com.mredrock.cyxbs.widget.widget.little.LittleWidget"
 const val littleWidgetAction = "$littleWidgetPkg.init"
 val widgetList = mapOf(
     littleWidgetAction to littleWidgetPkg
-
 )
