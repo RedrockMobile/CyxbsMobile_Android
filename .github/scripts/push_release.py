437527d69250c0c85c75c9750243d0ca36487c32
ArcticRain <rain@redrock.team>
2023-01-08 19:34:32 +0800

    :construction_worker: bug fix

--- .github/scripts/push_release.py
+++ .github/scripts/push_release.py
@@ -8,10 +8,19 @@ if os.path.exists("./release_json/release.json"):
     # 跟网络上的最新数据比对
     with open("./release_json/release.json") as f:
         info = json.loads(f.read())
-    content_json = info.content_json
+        print("本地数据: ")
+        print(info)
+    content_json = info["content_json"]
     latest = requests.get(f"{base_url}/cyxbsAppUpdate.json").json()
+    print("云端数据: ")
+    print(latest)
     # 不相同说明有更新
     if latest.version_code != content_json.version_code:
         # 检查是否到时间了，到了就直接更新json
-        if time.time() > info.update_time: 
+        if time.time() > info["update_time"]: 
+            print("触发发版操作")
             print(requests.post(f"{base_url}/cyxbsAppUpdate.json", json=content_json, headers={ "token": token }).json())
+        else:
+            print("没到发版时间")
+    else:
+        print("已是最新版本")
