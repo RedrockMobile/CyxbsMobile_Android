95ea11422449a512e98865b19b6f2923a4c2837e
ArcticRain <rain@redrock.team>
2023-01-09 15:52:36 +0800

    :sparkles: 新建module_declare 表态模块

---
+++ module_declare/src/main/java/com/mredrock/cyxbs/declare/pages/post/PostActivity.kt
@@ -0,0 +1,10 @@
+package com.mredrock.cyxbs.declare.pages.post
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
