72c22d59fa163e01afc331566f1f165e1d3e662d
ArcticRain <rain@redrock.team>
2023-02-15 00:06:25 +0800

    :sparkles: 简单完成表态发布页

---
+++ module_declare/src/main/java/com/mredrock/cyxbs/declare/pages/post/PostViewModel.kt
@@ -0,0 +1,11 @@
+package com.mredrock.cyxbs.declare.pages.post
+
+/**
+ * com.mredrock.cyxbs.declare.pages.post.PostViewModel.kt
+ * CyxbsMobile_Android
+ *
+ * @author 寒雨
+ * @since 2023/2/8 下午3:28
+ */
+class PostViewModel {
+}
---
+++ module_declare/src/main/java/com/mredrock/cyxbs/declare/pages/post/adapter/PostSectionRvAdapter.kt
@@ -0,0 +1,11 @@
+package com.mredrock.cyxbs.declare.pages.post.adapter
+
+/**
+ * com.mredrock.cyxbs.declare.pages.post.adapter.PostSectionRvAdapter.kt
+ * CyxbsMobile_Android
+ *
+ * @author 寒雨
+ * @since 2023/2/8 下午4:56
+ */
+class PostSectionRvAdapter {
+}
