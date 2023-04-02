b12af2c5686a42f3dac108ea8918bb22e5cd783a
why <92087542+whyStrive@users.noreply.github.com>
2023-04-02 09:40:33 +0800

    :sparkles: 登陆后上传登录使用的设备信息

---
+++ module_login/src/main/java/com/mredrock/cyxbs/login/network/LoginApiService.kt
@@ -0,0 +1,26 @@
+package com.mredrock.cyxbs.login.network
+
+import com.mredrock.cyxbs.lib.utils.network.ApiGenerator
+import com.mredrock.cyxbs.lib.utils.network.ApiStatus
+import com.mredrock.cyxbs.login.bean.DeviceInfoParams
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+/**
+ * @author : why
+ * @time   : 2023/4/1 11:19
+ * @bless  : God bless my code
+ */
+interface LoginApiService {
+    companion object {
+        val INSTANCE by lazy {
+            ApiGenerator.getApiService(LoginApiService::class)
+        }
+    }
+
+    /**
+     * 登录时上传设备信息以及wifi的ip地址
+     */
+    @POST("/magipoke/token/record")
+    fun recordDeviceInfo(@Body deviceInfoParams: DeviceInfoParams):ApiStatus
+}
