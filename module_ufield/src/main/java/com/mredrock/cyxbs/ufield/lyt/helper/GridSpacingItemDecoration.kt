4921896195a8eba2205fd6df5985fb27a8f8014d
lytMoon <2936437824@qq.com>
2023-08-31 17:14:29 +0800

    :sparkles: 升级代码+8

--- module_ufield/src/main/java/com/mredrock/cyxbs/ufield/lyt/helper/GridSpacingItemDecoration.kt
+++ module_ufield/src/main/java/com/mredrock/cyxbs/ufield/lyt/helper/GridSpacingItemDecoration.kt
@@ -3,8 +3,10 @@ package com.mredrock.cyxbs.ufield.lyt.helper
 import android.content.Context
 import android.graphics.Rect
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import java.lang.Math.ceil
 
 /**
  *  description :用于辅助Rv行列的分布。控制间距大小
@@ -41,11 +43,36 @@ class GridSpacingItemDecoration(
         }
 
         val position = parent.getChildAdapterPosition(view)
+        val spanCount = 2  // 假设是两列的布局
+        val itemCount = parent.adapter?.itemCount ?: 0
+        //因为有奇数的可能，所以向上取整
+        val rowCount = kotlin.math.ceil(itemCount.toDouble() / spanCount).toInt()
+        Log.d("965966", "测试结果-->> ${rowCount}");
+
+        // 计算当前item所在的行数和列数
+        val row = position / spanCount
         val column = position % spanCount
 
-        outRect.top = spacingPx
-        outRect.right = if (column == 1) spacingLRDp else spacingLRDp / 2
-        outRect.left = if (column == 1) spacingLRDp / 2 else spacingLRDp
+
+
+        // 如果是最后一行，则设置底部间距
+        if (row == rowCount - 1) {
+            outRect.bottom = 80
+        }
+
+        // 根据列数设置相应的间距
+        if (column == 0) {
+            // 第一列
+            // 设置左侧间距
+            outRect.left = 50
+            outRect.right = 5
+        } else {
+            // 第二列
+            // 设置右侧间距
+            outRect.left = 5
+            outRect.right = 50
+        }
+
     }
 
     private fun dpToPx(context: Context, dp: Int): Int {
