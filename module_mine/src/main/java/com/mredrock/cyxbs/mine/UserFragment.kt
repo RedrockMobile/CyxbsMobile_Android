ceb72904594ae49def5ac12ffec8a3b3ec67abc4
Black-Skyline <2031649401@qq.com>
2023-09-03 21:06:00 +0800

    :zap: 本次没有本地逻辑改动，主要是上次push时未同步合并develop分支最新代码，本次提交之前已合并develop分支

--- module_mine/src/main/java/com/mredrock/cyxbs/mine/UserFragment.kt
+++ module_mine/src/main/java/com/mredrock/cyxbs/mine/UserFragment.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -387,7 +386,7 @@ class UserFragment : BaseFragment() {
                 }
             }
         })
-
+        // 先注册观察者再请求数据
         viewModel.getNewNotificationCount()
     }
 }
