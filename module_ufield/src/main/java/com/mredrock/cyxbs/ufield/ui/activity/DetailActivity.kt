9e83b0dda8e4159f876581eabbb5de552b6bf72c
古云顿 <2191288460@qq.com>
2023-09-09 16:11:18 +0800

    :bug: 更改了资源文件类型，修改了未通过和未审核的活动详情ui

--- module_ufield/src/main/java/com/mredrock/cyxbs/ufield/ui/activity/DetailActivity.kt
+++ module_ufield/src/main/java/com/mredrock/cyxbs/ufield/ui/activity/DetailActivity.kt
@@ -138,10 +138,23 @@ class DetailActivity : BaseActivity() {
             ivCover.setImageFromUrl(it.activityCoverUrl)
             tvStart.text = trans(it.activityStartAt)
             tvEnd.text = trans(it.activityEndAt)
-            if (it.activityState != "published") {
-                layout.gone()
-                tvGoing.gone()
-                ivGoing.gone()
+            if (it.activityState == "reviewing") {
+               setStatusText("活动未审核")
+            }else if(it.activityState=="rejected"){
+               setStatusText("活动未通过")
+            }else{
+                if (it.ended) {
+                    tvGoing.text = "已结束"
+                    ivGoing.setImageResource(R.drawable.ufield_ic_finished)
+                    tvTime.text = "活动已结束"
+                    tvDays.gone()
+                    tvHours.gone()
+                    tvMinutes.gone()
+                    tvSeconds.gone()
+                    layout.gone()
+                } else {
+                    startDownTimer(it.activityStartAt)
+                }
             }
             if (it.wantToWatch) {
                 //在将文本替换为“已想看”后，修改文本位置，使其位于中心
@@ -155,18 +168,7 @@ class DetailActivity : BaseActivity() {
                     viewModel.wantToSee(id)
                 }
             }
-            if (it.ended) {
-                tvGoing.text = "已结束"
-                ivGoing.setImageResource(R.drawable.ufield_ic_finished)
-                tvTime.text = "活动已结束"
-                tvDays.gone()
-                tvHours.gone()
-                tvMinutes.gone()
-                tvSeconds.gone()
-                layout.gone()
-            } else {
-                startDownTimer(it.activityStartAt)
-            }
+
         }
 
     }
@@ -229,6 +231,17 @@ class DetailActivity : BaseActivity() {
         }
     }
 
+    private fun setStatusText(text:String){
+        layout.gone()
+        tvGoing.gone()
+        ivGoing.gone()
+        tvTime.text = text
+        tvDays.gone()
+        tvHours.gone()
+        tvMinutes.gone()
+        tvSeconds.gone()
+        layout.gone()
+    }
 
     private fun trans(timestampInSeconds: Long): String {
         val date = Date(timestampInSeconds * 1000L)
