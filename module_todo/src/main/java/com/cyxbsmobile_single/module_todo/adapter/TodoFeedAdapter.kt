328816a3b0943ce2a9b30561710aaab846d6dad1
you <2719757561@qq.com>
2024-08-24 22:06:08 +0800

    :sparkles: 修改了首页邮子清单的过时显色问题

--- module_todo/src/main/java/com/cyxbsmobile_single/module_todo/adapter/TodoFeedAdapter.kt
+++ module_todo/src/main/java/com/cyxbsmobile_single/module_todo/adapter/TodoFeedAdapter.kt
@@ -1,5 +1,6 @@
 package com.cyxbsmobile_single.module_todo.adapter
 
+import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -12,8 +13,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cyxbsmobile_single.module_todo.R
 import com.cyxbsmobile_single.module_todo.component.CheckLineView
 import com.cyxbsmobile_single.module_todo.model.bean.Todo
+import com.cyxbsmobile_single.module_todo.ui.activity.TodoDetailActivity.Companion.startActivity
 import com.mredrock.cyxbs.lib.utils.extensions.gone
 import com.mredrock.cyxbs.lib.utils.extensions.visible
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 /**
  * description:
@@ -35,6 +40,8 @@ class TodoFeedAdapter :
         }
     }
 
+    // 定义日期格式
+    private val dateFormat = SimpleDateFormat("yyyy年MM月dd日HH:mm", Locale.getDefault())
     private var mClick: ((Todo) -> Unit)? = null
     fun onFinishCheck(listener: (Todo) -> Unit) {
         mClick = listener
@@ -59,13 +66,26 @@ class TodoFeedAdapter :
                             R.color.todo_check_item_color
                         )
                     )
+                    todoFeedTime.setTextColor(
+                        ContextCompat.getColor(
+                            itemView.context,
+                            R.color.todo_check_item_color
+                        )
+                    )
+                    todoFeedIv.setImageResource(R.drawable.todo_ic_addtodo_notice2)
                     icRight.visible()
                 }
+            }
+            itemView.setOnClickListener {
+                startActivity(currentList[absoluteAdapterPosition],itemView.context)
             }
         }
         fun bind(todo: Todo) {
             todoTitle.text = todo.title
-            defaultCheckbox.setStatusWithAnime(false)
+            defaultCheckbox.apply {
+                setStatusWithAnime(false)
+                uncheckedColor = com.mredrock.cyxbs.config.R.color.config_level_two_font_color
+            }
             todoTitle.setTextColor(
                 ContextCompat.getColor(
                     itemView.context,
@@ -79,6 +99,24 @@ class TodoFeedAdapter :
                 todoFeedTime.gone()
             } else {
                 todoFeedTime.text = todo.remindMode.notifyDateTime
+                val itemTime = if (!todo.remindMode.notifyDateTime.isNullOrEmpty()) {
+                    try {
+                        dateFormat.parse(todo.remindMode.notifyDateTime)?.time ?: 0L
+                    } catch (e: ParseException) {
+                        // 如果解析失败，打印错误并使用一个默认时间值，例如当前时间
+                        e.printStackTrace()
+                        System.currentTimeMillis()
+                    }
+                } else {
+                    0L
+                }
+                val currentTime = System.currentTimeMillis()
+                if (currentTime > itemTime && itemTime != 0L){
+                    defaultCheckbox.uncheckedColor = Color.RED
+                    todoTitle.setTextColor(Color.RED)
+                    todoFeedTime.setTextColor(Color.RED)
+                    todoFeedIv.setImageResource(R.drawable.todo_ic_addtodo_overtime_notice)
+                }
             }
         }
     }
